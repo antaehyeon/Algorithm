33b3139a50bee758ef5eaa4b9e32ea2fca2269c8
안태현 <antaehyeon@live.co.kr>
2017-10-08 22:17:56 +0900

    백준 알고리즘 스택 (문제번호 10828)에 대한 풀이입니다.
    겪었던 점
    1. #define 과 const 의 차이점
    2. 배열을 동적할당으로 선언할 때 크기를 어떻게 알 수 있는가?
    sizeof(array) / sizeof(*array) = 0
    _msize(array) / sizeof(*array) = 처음에 동적할당했던 크기 나옴
    3. getline , cin.get()
    엔터기호에 대한 처리 알아두기
    getline 에서는 엔터기호까지 먹기때문에 엔터기호('\n')를 따로 처리할 필요가 없지만
    cin 으로 한번 받은 다음에 getline을 해버리면 getline에서 엔터기호('\n')를 받아버리는 점 주의

--- 10828/10828.cpp
+++ 10828/10828.cpp
@@ -16,34 +16,43 @@ int main() {
 	// 변수 선언 부
 	int *STACK;
 	int inputDataNumber = 0;
+	int stackSize = 0;
+
+	string *commandArray;
 
 	// 입력
 	cin >> inputDataNumber;
 
 	// 동적할당
 	STACK = new int[inputDataNumber];
+	commandArray = new string[inputDataNumber];
+
+	cin.get();
 
 	for (int i = 0; i < inputDataNumber; i++) {
-		string command = "";
-		//cin >> command;
-		getline(cin, command);
-		
-		int commandType = CheckCommandType(command);
-		int stackSize = Size(STACK);
+		string command;
+		getline(cin, commandArray[i]);
+	} // for 1 - inputDataNumber
+
+	for (int i = 0; i < inputDataNumber; i++) {
+		int commandType = CheckCommandType(commandArray[i]);
+
 		switch (commandType)
 		{
 		case PUSH:
-			command.replace(0, 4, "");
-			STACK[stackSize] = atoi(command.c_str());
+			commandArray[i].replace(0, 4, "");
+			STACK[stackSize] = atoi(commandArray[i].c_str());
+			stackSize++;
 			break;
 		case POP:
 			if (stackSize == 0) {
 				cout << -1 << endl;
 				break;
 			}
-			command.replace(0, 3, "");
+			commandArray[i].replace(0, 3, "");
 			cout << STACK[stackSize - 1] << endl;
-			STACK[stackSize - 1] = NULL;
+			STACK[stackSize - 1] = -1;
+			stackSize--;
 			break;
 		case SIZE:
 			cout << stackSize << endl;
@@ -57,26 +66,28 @@ int main() {
 				cout << -1 << endl;
 				break;
 			}
-			cout << STACK[stackSize - 1] << endl;			
+			cout << STACK[stackSize - 1] << endl;
 			break;
 		default:
 			break;
-		}
+		} // switch
 	}
 
-	system("pause");
+	//system("pause");
 
 	return 0;
-}
+} // main
 
 int CheckCommandType(string command) {
-	if (command.find("push") == 1)			return PUSH;
-	else if (command.find("pop") == 1)		return POP;
-	else if (command.find("size") == 1)		return SIZE;
-	else if (command.find("empty") == 1)	return EMPTY;
-	else if (command.find("top") == 1)		return TOP;
+	if (command.find("push") == 0)			return PUSH;
+	else if (command.find("pop") == 0)		return POP;
+	else if (command.find("size") == 0)		return SIZE;
+	else if (command.find("empty") == 0)	return EMPTY;
+	else if (command.find("top") == 0)		return TOP;
+	else									return -1;
 }
 
 int Size(int *DataSet) {
-	return sizeof(DataSet) / sizeof(*DataSet);
+	//return sizeof(DataSet) / sizeof(*DataSet);	// 0
+	//return _msize(DataSet) / sizeof(*DataSet);		// 14
 }
