6b7dcfaae5d11bd8fac62260b1a28df96b5ea7b9
안태현 <antaehyeon@live.co.kr>
2017-10-30 20:44:08 +0900

    백준 알고리즘 '농구 경기(문제번호 1159)'에 대해 처음 작성한 코드입니다.

---
+++ Algorithm/Code.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	int nameCount[26] = { 0, };
+	bool check = false;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		char name[31];
+		cin >> name;
+		nameCount[name[0] - 97]++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (nameCount[i] >= 5) {
+			cout << (char)nameCount[i];
+			check = true;
+		}
+	}
+
+	if (!check) cout << "PREDAJA" << endl;
+
+	system("pause");
+
+	return 0;
+}
