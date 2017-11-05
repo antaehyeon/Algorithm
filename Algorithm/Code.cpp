0062b98749ac457066f784edf86731daf27cec43
안태현 <antaehyeon@live.co.kr>
2017-11-05 22:14:35 +0900

    백준 알고리즘 '방 번호(문제번호 1475)'에 대해 처음 작성한 코드입니다. (오답)
    0~9까지의 배열을 선언해서 각각 나오는 문자에 대해, 해당 index로 인식시켜 증가시킨후에 최종적으로 max값을 뽑아내는 식으로 구현하였습니다.
    그러나 문제 중 6과 9를 바꿔쓸수 있다는 부분에 대해서 9로 들어오는 문자열을 전부 6으로 변환시킨후 0~8 배열로만 구성한 후
    최대값이 6이 될 경우에 2를 나눠주어 출력하게 했습니다.
    
    홀수가 될 경우 카드가 더 필요한데 오답이 날 수 있으므로 + 1을 해줬으나, 짝수인경우에는 괜히 +1이 되어 오답이 나오는 것 같습니다.

--- Algorithm/Code.cpp
+++ Algorithm/Code.cpp
@@ -6,45 +6,35 @@
 #include <vector>
 #include <tuple>
 #include <utility>
-#define VSIZE 100000
+#include <stdlib.h>
+#define VSIZE 1000000
 
 using namespace std;
 
-struct Student {
-	string name;
-	int korean, english, math;
-	Student(int x = 0, int y = 0, int z = 0):korean(x), english(y), math(z) {}
-};
+int visit[9] = { 0, };
 
 int main() {
-	// C++ 에서도 입출력에 대해 C만큼의 속도를 낼 수 있는 코드
-	// 그러나, scanf, printf 는 사용 불가능
-	ios_base::sync_with_stdio(false);
 
-	int n;
-	cin >> n;
+	char strArr[VSIZE + 1];
+	int x	  = 0,
+		index = 0,
+		max	  = 0;
 
-	vector<Student> v(n);
+	cin >> strArr;
 
-	for (auto &x : v) cin >> x.name >> x.korean >> x.english >> x.math;
-	
-	// 람다함수 적용
-	//1. 국어 점수가 감소하는 순서로 = 국어점수가 다르면
-	//2. 국어 점수가 같으면 영어 점수가 증가하는 순서로 = 영어점수가 다르면
-	//3. 국어 점수와 영어 점수가 같으면 수학 점수가 감소하는 순서로 = 수학점수가 다르면
-	//4. 모든 점수가 같으면 이름이 사전 순으로 증가하는 순서로
-	sort(v.begin(), v.end(), [](const Student &x, const Student &y) -> bool {
-		if (x.korean != y.korean) return x.korean > y.korean; // 내림차순
-		if (x.english != y.english) return x.english < y.english; // 오름차순
-		if (x.math != y.math) return x.math > y.math; // 내림차순
-		return x.name < y.name; // 내림차순
-	});
+	while (strArr[index] != '\0') {
+		if (strArr[index] == '9')
+			strArr[index] = '6';
+		visit[(int)strArr[index++] - 48]++;
+	}
 
-	// C++11, foreach문
-	// endl보다 \n이 훨씬 빠르기 때문에 대신사용
-	for (auto &x : v) cout << x.name << '\n';
+	for (auto i : visit)
+		if (i > max) max = i;
 
-	system("pause");
+	if (max == visit[5])
+		max = max / 2 + 1;
+
+	cout << max;
 
 	return 0;
 }
