94ead5b9202d4e70d55859437200b99b2ef1e88d
안태현 <antaehyeon@live.co.kr>
2017-11-14 05:32:08 +0900

    알고리즘 신입 공채 1차 코딩 테스트 3번째 문제(캐시)입니다.
    해당 조건에서 캐시 교체 알고리즘은 LRU(Least Recently Used)를 사용하라고 하였습니다.
    이에 대한 LRU 테스트 코드입니다.

--- Algorithm/Code.cpp
+++ Algorithm/Code.cpp
@@ -4,89 +4,23 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+#include "LRUCache.cpp"
 
 using namespace std;
 
 int main() {
 
-	vector<int> data;
-	vector<char> result;
+	LRUCache cache(2);  // cache capacity 2
+	cache.put(2, 2);
+	cout << cache.get(2) << endl;
+	cout << cache.get(1) << endl;
+	cache.put(1, 1);
+	cache.put(1, 5);
+	cout << cache.get(1) << endl;
+	cout << cache.get(2) << endl;
+	cout << cache.get(1) << endl;
+	cache.put(8, 8);
+	cout << cache.get(1) << endl;
+	cout << cache.get(8) << endl;
 
-	string dartResult = "";
-
-	int r = 0,
-		size = 0;
-
-	cin >> dartResult;
-
-	for (int i = 0; i < dartResult.length(); i++) {
-		int number = stoi(to_string(dartResult.at(i)));
-
-		// 0 ~ 9 魄喊扼牢
-		if (number >= 48 && number <= 57) {
-			result.push_back(number - 48);
-			// 父距 10老版快
-			if (stoi(to_string(dartResult.at(i + 1))) == 48) {
-				result.pop_back();
-				result.push_back(10);
-				i++;
-			}
-			continue;
-		}
-		
-		// S老版快
-		if (number == 83) continue;
-
-		// D老版快
-		if (number == 68) {
-			int d = result.back();
-			result.pop_back();
-			result.push_back(pow(d, 2));
-			continue;
-		}
-		// T老版快
-		if (number == 84) {
-			int d = result.back();
-			result.pop_back();
-			result.push_back(pow(d, 3));
-			continue;
-		}
-		// *老版快
-		if (number == 42) {
-			int n = result.size();
-			if (n == 1) {
-				int d = result.back();
-				result.pop_back();
-				result.push_back(d * 2);
-				continue;
-			}
-			int d = result.back();
-			result.pop_back();
-			int d2 = result.back();
-			result.pop_back();
-			d2 *= 2;
-			result.push_back(d2);
-			d *= 2;
-			result.push_back(d);
-			continue;
-		}
-		// #老版快
-		if (number == 35) {
-			int d = result.back();
-			result.pop_back();
-			result.push_back(-d);
-			continue;
-		}
-	}
-
-	size = result.size();
-
-	for (int i = 0; i < size; i++) {
-		r += result.back();
-		result.pop_back();
-	}
-
-	cout << r << endl;
-
-	return 0;
 }
