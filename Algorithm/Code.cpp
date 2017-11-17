ffd9342cb17313b7d1c31b1870aa7757933aba5c
안태현 <antaehyeon@live.co.kr>
2017-11-17 23:58:03 +0900

    백준 알고리즘 'A+B-4(문제번호 10951)'에 대해 처음 작성한 코드입니다.
    입력을 모두 하였지만, 계속해서 입력대기상태로 빠집니다. while 문에서 scanf 의 반환값으로 2를 검사하는 부분을 END 시킬 수 있는 방법을 찾아볼 계획입니다.

--- Algorithm/Code.cpp
+++ Algorithm/Code.cpp
@@ -1,41 +1,21 @@
 /*
-DP 프로그래밍 입문
-(1로 만들기:1463) https://www.acmicpc.net/problem/1463
+A+B
+A+B - 4 (https://www.acmicpc.net/problem/10951)
 */
 
 #include <iostream>
-#include <algorithm>
-#define SIZE 10000000
 
 using namespace std;
 
-int d[SIZE + 1] = { 0, };
-
-int go(int n) {
-
-	if (n == 1) return 0;
-	if (d[n] > 0) return d[n];
-
-	d[n] = go(n - 1) + 1;
-
-	if (n % 2 == 0) {
-		int temp = go(n / 2) + 1;
-		if (d[n] > temp) d[n] = temp;
-	}
-	if (n % 3 == 0) {
-		int temp = go(n / 3) + 1;
-		if (d[n] > temp) d[n] = temp;
-	}
-
-	return d[n];
-}
-
 int main() {
 
-	int n;
-	cin >> n;
+	int a = 0,
+		b = 0;
 
-	cout << go(n) << endl;
+	cin >> a >> b;
+
+	cout << a + b;
 
 	return 0;
+
 }
