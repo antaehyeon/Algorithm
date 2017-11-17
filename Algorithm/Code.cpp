398814762294423d034926bb14dcbba1e977cdf5
안태현 <antaehyeon@live.co.kr>
2017-11-17 23:59:04 +0900

    백준 알고리즘 'A+B-4(문제번호 10951)'에 대해 처음 작성한 코드입니다.
    전 Commit 수정본 입니다.

--- Algorithm/Code.cpp
+++ Algorithm/Code.cpp
@@ -4,6 +4,7 @@ A+B - 4 (https://www.acmicpc.net/problem/10951)
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -12,9 +13,17 @@ int main() {
 	int a = 0,
 		b = 0;
 
-	cin >> a >> b;
+	//vector <int> v;
 
-	cout << a + b;
+	//while (scanf("%d %d", &a, &b) == 2) {
+
+	//	v.push_back(a + b);
+	//}
+
+	//for (auto i : v) cout << i << endl;
+
+	while (scanf("%d %d", &a, &b) == 2)
+		cout << a + b << endl;
 
 	return 0;
 
