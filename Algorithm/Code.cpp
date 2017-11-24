fad5cfaa1d44824d4e18e2487116977522cf55c3
안태현 <antaehyeon@live.co.kr>
2017-11-24 20:19:55 +0900

    백준 알고리즘 '8진수, 10진수, 16진수(문제번호 11816)'에 대해 완성된 코드입니다.
    - scanf중 %i 를 이용해서 출력하였습니다

--- Algorithm/Code.cpp
+++ Algorithm/Code.cpp
@@ -1,36 +1,17 @@
 /*
-A+B
-A+B - 4 (https://www.acmicpc.net/problem/10951)
+8진수, 10진수, 16진수
+https://www.acmicpc.net/problem/11816
 */
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
-
-bool cmp(const int &a, const int &b) {
-	return a < b;
-}
 
 int main() {
 
-	int n;
+	int x;
 
-	vector <int> v;
+	scanf("%i", &x);
 
-	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++) {
-		int a;
-		scanf("%d", &a);
-		v.push_back(a);
-	}
-
-	sort(v.begin(), v.end(), cmp);
-
-	for (int i = 0; i < n; i++) {
-		printf("%d\n", v[i]);
-	}
+	cout << x;
 
 }
