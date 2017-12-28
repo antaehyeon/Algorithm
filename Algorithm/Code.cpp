6c2dafd666144c4434aa626935f3325f4e6faf61
HYEON <antaehyeon@live.co.kr>
2017-12-29 00:40:56 +0900

    Update README.md - 순환(Recursion)의 개념과 기본예제 1 내용 추가

--- Algorithm/Code.cpp
+++ Algorithm/Code.cpp
@@ -1,105 +1,77 @@
-/*
-NHN Ent 2017 Pre-Test 1차
-암복호화
-*/
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string.h>
+#include <cstring>
 #include <string>
+
 using namespace std;
 
-char sumAlphabet(char a, char b) {
+/*
+* Description :
+* Time complexity :
+* Space complexity :
+*/
+bool match(string &pat, string &s) {
+	// TODO implement your codes to here.
 
-	int n = (a - 97) + (b - 97);
-	
-	if (n > 26) n -= 26;
-	
-	return (char)n + 97;
+	if (pat.compare(s) == 0) return true;
+	else {
+
+		vector<char> a;
+		vector<char> b;
+
+		char letter;
+
+		for (auto c : pat) {
+			a.push_back(c);
+		}
+
+		for (auto c : s) {
+			b.push_back(c);
+		}
+
+		auto findQ = find(a.begin(), a.end(), '?');
+		auto findS = find(a.begin(), a.end(), '*');
+
+		if (findQ != a.end()) {
+			for (int i = 0; i < pat.size(); i++) {
+				if (!(pat[i] == s[i]))
+					if (pat[i] == '?') continue;
+					else return false;
+			}
+			return true;
+		}
+
+		else if (findS != a.end()) {
+			for (int i = 0; i < a.size(); i++) {
+				auto index = distance(a.begin(), findS);
+				letter = a[index + 1];
+				a.erase(a.begin() + index,a.begin() + index + 2);
+			}
+			int bSize = b.size();
+			for (int i = 0; i < bSize-1; i++) {
+				auto index = distance(b.begin(), find(b.begin(), b.end(), letter));
+				b.erase(b.begin() + index);
+			}
+
+			for (int i = 0; i < a.size(); i++) {
+				if (a[i] != b[i]) {}
+				else return true;
+			}
+
+		}
+		else return false;
+	}
 }
 
-char minusAlphabet(char a, char b) {
-	int n = (a - 97) - (b - 97);
-	
-	if (n < 0) {
-		n += 26;
+int main(int argc, const char *argv[]) {
+	int TC;
+	string pattern, target_string;
+	cin >> TC;
+	for (int t = 0; t < TC; t++) {
+		cin >> pattern >> target_string;
+		cout << (match(pattern, target_string) ? 'O' : 'X') << endl;
 	}
-
-	return (char)n + 97;
-}
-
-int main() {
-
-	string s = "",
-		mode = "",
-		secretKey = "",
-		message = "",
-		temp = "";
-
-	int rotateNum = 0,
-		stringMode = 0;
-
-	vector <char> answer;
-
-	getline(cin, s);
-
-	for (auto c : s) {
-		if (c == ' ') {
-			stringMode++;
-			continue;
-		}
-
-		switch (stringMode)
-		{
-		case 0:
-			mode += c;
-			break;
-		case 1:
-			secretKey += c;
-			break;
-		case 2:
-			rotateNum = c - 48;
-			break;
-		case 3:
-			message += c;
-			break;
-		default:
-			break;
-		}
-	}
-
-
-
-	if (mode == "encrypt") {
-		for (int i = 0; i < message.length(); i++) {
-			char c = sumAlphabet(secretKey[i], message[i]);
-
-			answer.push_back(c);
-		}
-		rotateNum %= message.length();
-		
-		rotate(answer.begin(), answer.begin() + rotateNum, answer.end());
-
-		for (auto c : answer) {
-			cout << c;
-		}
-	}
-	else if (mode == "decrypt") {
-
-		rotateNum %= message.length();
-
-		rotate(message.rbegin(), message.rbegin() + rotateNum, message.rend());
-
-		for (int i = 0; i < message.length(); i++) {
-			char c = minusAlphabet(message[i], secretKey[i]);
-
-			answer.push_back(c);
-		}
-
-		for (auto c : answer) {
-			cout << c;
-		}
-	}	
-
 	return 0;
 }
--- README.md
+++ README.md
@@ -48,9 +48,9 @@ function<int(int)> f = [](int n) {
 위와 같이 auto가 아닌 function<리턴타입(자료형, ...)>으로 사용해야 한다
 ```
 
--	[람다표현식의 장점](http://qwefgh90.github.io/java/Lambda-Expression/)
+-  [람다표현식의 장점](http://qwefgh90.github.io/java/Lambda-Expression/)
 
--	[사칙 연산](https://www.acmicpc.net/problem/10869)
+  -[사칙 연산](https://www.acmicpc.net/problem/10869)
 
 ```
 vector<function<int(int, int)>> d;
@@ -91,3 +91,63 @@ rotate(q.rbegin(), q.rbegin()+num, q.rend());
 vector 와 string 을 이용하면 특정문자열로 찢기가 편함
 그리고 string 하나 선언해서 문자열을 붙이면 원하는 문자열도 생성가능
 ```
+
+- # 인프런 알고리즘 강좌 (권오흠)
+
+- ## 순환(Recursion)의 개념과 기본예제1
+
+- Recursion (재귀함수)
+
+  - 자기 자신을 호출하는 함수
+
+  - Base case : 적어도 하나의 Recursion에 빠지지 않는 경우가 존재해야 한다
+
+  - Recursive case : Recursion을 반복하다보면 결국 base case로 수렴해야 한다
+
+  -  Factorial : n!
+
+    - 0! = 1
+    - n! = n*(n-1)! n>0
+
+  - x^n
+
+    - x^0 = 1
+    - x^n = x*x^(n-1)  if n>0
+
+  - Fibonacci Number
+
+    - f0 = 0
+    - f1 = 1
+    - fn = fn-1 + fn-2  n>1
+
+  - 최대공약수 : Euclid Method
+
+    ```
+    m>=n인 두 양의 정수 m과 n에 대해서 m이 n의 배수이면 gcd(m,n) = n이고, 그렇지 않으면 gcd(m,n) = gcd(n, m%n)이다.
+    ```
+
+    ```
+    public static double gcd(int m, int n) {
+      if (m<n) {
+        int tmp=m; m=n; n=tmp;		// swap m and n
+      }
+      if (m%n == 0)
+      	return n;
+      else
+      	return gcd(n, m%n);
+    }
+    ```
+
+    ```
+    gcd(p, q) = if q=0, p
+    			otherwise, gcd(q, p%q)
+    			
+    public static int gcd(int p, int q) {
+      if (q==0)
+      	return p;
+      else
+      	return gcd(q, p%q);
+    }
+    ```
+
+    ​
