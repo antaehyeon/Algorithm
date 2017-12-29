05a033001a6e7d235e09de5e2373a98d413c9cc3
HYEON <antaehyeon@live.co.kr>
2017-12-30 04:49:31 +0900

    Add Java Project(Algorithm) - 알고리즘 자바로 언어변경 - 백준 알고리즘 'Hello World(문제번호 2557)'에 대해 완성된 코드입니다.

---
+++ Algorithm(JAVA)/src/main/java/Main.java
@@ -0,0 +1,9 @@
+// Hello World (문제번호 2557)
+// https://www.acmicpc.net/problem/2557
+
+public class Main {
+    public static void main(String args[]) {
+
+        System.out.println("Hello World!");
+    }
+}
