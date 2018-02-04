ad560e666ab88771d078dee3216784166a742024
HYEON <antaehyeon@live.co.kr>
2018-02-04 13:28:51 +0900

    백준 알고리즘 '저항 (문제번호 1076)' JAVA 코딩 (완료) - 정수형 계산의 출력은 정수형으로

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,32 @@
 package com.company;
 
 import java.io.*;
-import java.util.TreeSet;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        TreeSet<String> treeset = new TreeSet<>();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
 
-        for (int i=0; i<n; i++) {
-            String line = bf.readLine();
-            String[] lines = line.split(" ");
-            String name = lines[0];
-            String type = lines[1];
-            if (type.equals("enter")) {
-                treeset.add(name);
-            } else if (type.equals("leave")) {
-                treeset.remove(name);
-            }
-        } // for i-n
+        HashMap<String, Integer> hmap = new HashMap<>();
+        HashMap<String, Integer> innerMap = new HashMap<>();
+        String[] colorArray = {"black", "brown", "red", "orange", "yellow", "green", "blue",
+                               "violet", "grey", "white"};
 
-        int size = treeset.size();
-
-        for (int i=0; i<size; i++) {
-            String s = treeset.last();
-            System.out.println(s);
-            treeset.remove(s);
+        int n = 1;
+        for (int i=0; i<10; i++) {
+            hmap.put(colorArray[i], i);
+            innerMap.put(colorArray[i], n);
+            n *= 10;
         }
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = bf.readLine();
+        String b = bf.readLine();
+        String c = bf.readLine();
+        long ans = hmap.get(a)*10 + hmap.get(b);
+        ans *= innerMap.get(c);
+
+        System.out.println(ans);
+
     } // MAIN
 }
