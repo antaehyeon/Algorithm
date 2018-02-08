bbe785bc268341b4398597afbbe5ce7d52cc4e46
HYEON <antaehyeon@live.co.kr>
2018-02-08 13:02:26 +0900

    백준 알고리즘 '최대 힙 (문제번호 11279)' JAVA 코딩 (544MS) - Compare 함수 이용

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,17 @@ package com.company;
 import java.util.*;
 import java.io.*;
 public class Main {
+    static class Compare implements Comparator<Integer> {
+        public int compare(Integer one, Integer two){
+            return two.compareTo(one);
+        }
+    }
+
     public static void main(String args[]) throws IOException {
-        PriorityQueue<Integer> p = new PriorityQueue<>();
+        Compare cmp = new Compare();
+        PriorityQueue<Integer> p = new PriorityQueue<>(1, cmp);
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
         int a = Integer.parseInt(bf.readLine());
         for(int i=0; i<a; i++) {
             int b = Integer.parseInt(bf.readLine());
