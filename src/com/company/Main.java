5cc4f6aad603a59da9ca4cfe35783c7a9e207f39
HYEON <antaehyeon@live.co.kr>
2018-02-08 12:46:55 +0900

    백준 알고리즘 '최소 힙 (문제번호 1927)' JAVA 코딩 (536MS) - PriorityQueue 는 우선순위가 높은 순서대로 출력됨

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,45 +4,16 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String args[]) throws IOException {
-        LinkedList<String> a = new LinkedList<>();
+        PriorityQueue<Integer> p = new PriorityQueue<>();
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(bf.readLine());
-        for (int i=0; i<n; i++) {
-            String[] lines = bf.readLine().split(" ");
-            switch (lines[0]) {
-                case "push":
-                    a.offer(lines[1]);
-                    break;
-                case "pop":
-                    if (a.isEmpty()) {
-                        System.out.println("-1");
-                        break;
-                    }
-                    System.out.println(a.poll());
-                    break;
-                case "size":
-                    System.out.println(a.size());
-                    break;
-                case "empty":
-                    if(a.isEmpty()) System.out.println("1");
-                    else            System.out.println("0");
-                    break;
-                case "front":
-                    if (a.isEmpty()) {
-                        System.out.println("-1");
-                        break;
-                    }
-                    System.out.println(a.peekFirst());
-                    break;
-                case "back":
-                    if (a.isEmpty()) {
-                        System.out.println("-1");
-                        break;
-                    }
-                    System.out.println(a.peekLast());
-                    break;
-            } // switch
-        } // for i-n
+        int a = Integer.parseInt(bf.readLine());
+        for(int i=0; i<a; i++) {
+            int b = Integer.parseInt(bf.readLine());
+            if (b==0)
+                if (p.isEmpty()) System.out.println("0");
+                else System.out.println(p.poll());
+            else
+                p.offer(b);
+        } // for i-a
     } // Main
 }
