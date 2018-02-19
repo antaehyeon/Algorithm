ea8ab01224ff03c9c6cf7cd4ab8642bc71267a26
HYEON <antaehyeon@live.co.kr>
2018-02-19 23:44:35 +0900

    백준 알고리즘 '큰 수 A+B' (문제번호 10757)' Java Scanner 이용 (18760KB, 320MS)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,14 @@
 package com.company;
 
 import java.math.BigInteger;
-import java.util.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        BigInteger[] d = new BigInteger[Math.max(n+1, 2)];
-        d[0] = BigInteger.ZERO;
-        d[1] = BigInteger.ONE;
-        for (int i=2; i<=n; i++) {
-            d[i] = d[i-1].add(d[i-2]);
-        }
-        System.out.println(d[n]);
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
     }
 }
