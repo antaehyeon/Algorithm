d038249081aa0e36ffd6d53ff89dd554d2d1bffb
HYEON(win) <antaehyeon@live.co.kr>
2018-03-20 01:51:52 +0900

    [Add] ) 쇠막대기 (10799) JAVA 코딩 (13456KB, 272MS)

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -5,48 +5,39 @@ import java.util.Stack;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    public static int run(String data) {
+        String[] lines = data.split("");
+        Stack<String> s = new Stack<>();
+        int result = 0;
+        boolean c = false;
 
-        int n = Integer.parseInt(bf.readLine());
-
-        boolean chk = false;
-
-        Stack<String> a = new Stack<>();
-
-        for (int i=0; i<n; i++) {
-            String line = bf.readLine();
-            String[] lines = line.split("");
-
-            for (int j=0; j<line.length(); j++) {
-                if (lines[j].equals("(")) {
-                    a.push("(");
+        for (String a : lines) {
+            if (a.equals("(")) {
+                s.push("(");
+                c = true;
+            } else {
+                if (c) {
+                    s.pop();
+                    c = false;
+                    result += s.size();
                 } else {
-                    if (a.size() == 0) {
-                        System.out.println("NO");
-                        chk = true;
-                        break;
-                    }
-                    if (!a.pop().equals("(")) {
-                        System.out.println("NO");
-                        a.clear();
-                        chk = true;
-                        break;
-                    }
+                    s.pop();
+                    result += 1;
                 }
             }
-
-            if (chk) {
-                chk = false;
-                continue;
-            }
-
-            if (a.size() > 0) {
-                a.clear();
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
-            }
         }
+
+        return result;
+    }
+
+    public static void print(String data) {
+        System.out.println(data);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String line = bf.readLine();
+
+        System.out.println(run(line));
     }
 }
