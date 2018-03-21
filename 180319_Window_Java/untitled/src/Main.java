60857f431c70cba3b7bdffa4bd234e067a1e9dcc
안태현 <antaehyeon@live.co.kr>
2018-03-21 23:58:54 +0900

    [Add] 문자열 분석 (10820) JAVA 코딩 (틀림)

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -5,39 +5,14 @@ import java.util.Stack;
 
 public class Main {
 
-    public static int run(String data) {
-        String[] lines = data.split("");
-        Stack<String> s = new Stack<>();
-        int result = 0;
-        boolean c = false;
-
-        for (String a : lines) {
-            if (a.equals("(")) {
-                s.push("(");
-                c = true;
-            } else {
-                if (c) {
-                    s.pop();
-                    c = false;
-                    result += s.size();
-                } else {
-                    s.pop();
-                    result += 1;
-                }
-            }
-        }
-
-        return result;
-    }
-
     public static void print(String data) {
         System.out.println(data);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String line = bf.readLine();
+        int n = Integer.parseint(bf.readLine());
 
-        System.out.println(run(line));
+        print(n);
     }
 }
