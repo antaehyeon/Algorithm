9a189e5dc35af15c719409682eb3c921eb0ee2aa
HYEON(win) <antaehyeon@live.co.kr>
2018-03-28 11:58:29 +0900

    [Add] 문자열 분석 (10820) JAVA 로 풀이 (9704KB, 100MS)

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -1,18 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
-    public static void print(String data) {
-        System.out.println(data);
-    }
-
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseint(bf.readLine());
 
-        print(n);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null && line.length() != 0) {
+            char[] words = line.toCharArray();
+
+            int small = 0; int big = 0;
+            int space = 0; int number = 0;
+            int error = 0;
+
+            for (char a : words) {
+                if ('a' <= a && 'z' >= a) {
+                    small++;
+                }
+                else if ('A' <= a && 'Z' >= a) {
+                    big++;
+                }
+                else if (' ' == a) {
+                    space++;
+                }
+                else if ('0' <= a && '9' >= a) {
+                    number++;
+                }
+                else {
+                    error++;
+                }
+            }
+
+            String result = small + " " + big + " " + number + " " + space + "\n";
+            sb.append(result);
+        }
+
+        System.out.print(sb);
+        br.close();
     }
 }
