0c4041a3f5a50bf1306bce66510dab6ee2f8375b
HYEON(win) <antaehyeon@live.co.kr>
2018-03-28 15:30:24 +0900

    [Add] DFS와 BFS (1260) JAVA 로 풀이 (11320KB, 244MS)

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -5,18 +5,84 @@ import java.util.*;
 
 public class Main {
 
+    static int[][] graph;
+    static boolean visited[];
+    static int N;
+    static int E;
+    static int start;
+    static StringBuilder sb = new StringBuilder("");
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());       // 정점
+        E = Integer.parseInt(st.nextToken());       // 간선
+        start = Integer.parseInt(st.nextToken());   // 시작지점
 
-        String a = st.nextToken() + st.nextToken();
-        String b = st.nextToken() + st.nextToken();
+        graph = new int[1001][1001];        // 정점의갯수 N, 간선의갯수 M
+        visited = new boolean[10001];       // 간선의 갯수
 
-//        long result = Integer.parseInt(a) + Integer.parseInt(b);
-        long result = Long.parseLong(a) + Long.parseLong(b);
-        System.out.print(result);
+        int a, b;
+
+        for (int i=1; i<=E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            // 간선 데이터 Ex) 1 2
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
+            // 그래프 설정
+            graph[a][b] = graph[b][a] = 1;
+        }
+
+        DFS(start);
+
+        System.out.println(sb.toString().trim());
+        sb.delete(0, sb.length());
+
+        // 방문 체크 배열 초기화
+        for (int j=1; j<=N; j++) {
+            visited[j] = false;
+        }
+
+        BFS(start);
+        System.out.println(sb.toString().trim());
+    }
+
+    public static void DFS(int i) {
+
+        visited[i] = true;
+
+        sb.append(i + " ");
+
+        for (int j=0; j<=N; j++) {
+            if (graph[i][j] == 1 && !visited[j]) {
+                DFS(j);
+            }
+        }
+    }
+
+    public static void BFS(int i) {
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(i);     // 큐에 데이터 추가
+        visited[i] = true;  // 방문했다 체크
+
+        sb.append(i + " ");
+
+        int temp;
+        while(!q.isEmpty()) {
+            temp = q.poll();
+            for (int j=0; j<N+1; j++) {
+                if (graph[temp][j] == 1 && !visited[j]) {
+                    q.offer(j);
+                    visited[j] = true;
+                    sb.append(j + " ");
+                }
+            }
+        }
+
     }
 }
--- README/Java-문법정리.md
+++ README/Java-문법정리.md
@@ -22,4 +22,33 @@
 
   ```
 
-  ​
+- DFS / BFS
+
+  ![](https://cdn.namuwikiusercontent.com/s/1fe9246903b78fae07577b243a0b22791e02cb39640d5cbaae10d9849343b4ea6f162a9a677a5892fbf7819abd4ef7221ebd3608849cfb66793411fb5e6439514ed5c5e86db6d87a310ee3a249998ad2?e=1528159424&k=r3mCuuvSpH1FMcdeAaKDiw)
+
+- .trim() : 앞뒤 공백을 다 잘라버림
+
+- Queue
+
+  - offer [객체를 넣음]
+  - poll [큐에서 데이터를 꺼냄]
+  - peek [맨 아래의 객체를 반환함, 대신 큐에서 제거하진 않음]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
