01bf0681a36e34165007ed31f0dd741e5d4e40af
안태현 <antaehyeon@live.co.kr>
2018-03-30 01:59:06 +0900

    [Add] 단지 번호붙이기 (2667) JAVA 풀이 (9124KB, 104MS)
    http://stack07142.tistory.com/160 참고했습니다.

--- Algo(Surface)/src/Main.java
+++ Algo(Surface)/src/Main.java
@@ -1,35 +1,102 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
+class FastScanner {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+
+    FastScanner(InputStream i) throws Exception {
+        br = new BufferedReader(new InputStreamReader(i));
+    }
+
+    String next() throws Exception {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() throws Exception {
+        return Integer.parseInt(next());
+    }
+}
+
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static final int[] dRow = {0, -1, 0, 1};
+    static final int[] dCol = {-1, 0, 1, 0};
 
-        int N = 0; int M = 0;
+    static int N;
+    static int[][] map = new int[25][25];
+    static boolean[][] visited = new boolean[25][25];
+    static ArrayList<Integer> a = new ArrayList<Integer>();
+    static Vector<Integer> v = new Vector<Integer>();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder("<");
-        LinkedList<Integer> list = new LinkedList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static int DFS(int row, int col, int groupN, int cnt) {
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken()) -1;
+        cnt++;
+        visited[row][col] = true;
 
-        for (int i=1; i<=N; i++) {
-            list.add(i);
+        for (int i=0; i<4; i++) {
+            int nextRow = row + dRow[i];
+            int nextCol = col + dCol[i];
+            // 아래, 왼쪽, 위에, 오른쪽 순으로 탐색
+
+            if (nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= N) continue;
+            if (map[nextRow][nextCol] == 0) continue;
+            if (visited[nextRow][nextCol]) continue;
+
+            cnt = DFS(nextRow, nextCol, groupN, cnt);
         }
 
-        int index = 0;
+        return cnt;
+    }
 
-        while(!list.isEmpty()) {
-            index += M;
-            if (index >= list.size()) {
-                index %= list.size();
+    public static void main(String[] args) throws Exception {
+
+        FastScanner fs = new FastScanner(System.in);
+
+        // 입력
+
+        N = fs.nextInt();
+
+
+        for (int a=0; a<N; a++) {
+            String line = fs.next();
+            char[] splitData = line.toCharArray();
+
+            for (int b=0; b<N; b++) {
+                map[a][b] = splitData[b] - '0';
             }
-            sb.append(list.remove(index) + ", ");
         }
-        System.out.println(sb.toString().substring(0, sb.length()-2) + ">");
+
+        // 계산
+        int groupN = 0;
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if (!visited[i][j] && map[i][j] != 0) {
+                    // 요소가 있는데 방문을 안한경우 체크
+                    groupN++;
+                    int cnt = DFS(i, j, groupN, 0);
+
+                    v.add(cnt);
+                }
+            }
+        }
+
+        Collections.sort(v);
+
+        // 출력
+
+        System.out.println(groupN);
+        for (int data : v) {
+            System.out.println(data);
+        }
+
     }
 }
