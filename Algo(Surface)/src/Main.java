928a3370edd79787a413d6a3385e24afb1ed176d
안태현 <antaehyeon@live.co.kr>
2018-03-30 18:14:01 +0900

    [Add] 섬의 갯수 (4963) JAVA 풀이 (10360KB, 144MS)

--- Algo(Surface)/src/Main.java
+++ Algo(Surface)/src/Main.java
@@ -24,79 +24,69 @@ class FastScanner {
     int nextInt() throws Exception {
         return Integer.parseInt(next());
     }
+
+    String nextString() throws Exception {
+        String result = br.readLine();
+        return result;
+    }
 }
 
 public class Main {
 
-    static final int[] dRow = {0, -1, 0, 1};
-    static final int[] dCol = {-1, 0, 1, 0};
+    static final int[] dRow = {0, -1, 0, 1, -1, 1, -1, 1};
+    static final int[] dCol = {-1, 0, 1, 0, 1, 1, -1, -1};
 
-    static int N;
-    static int[][] map = new int[25][25];
-    static boolean[][] visited = new boolean[25][25];
-    static ArrayList<Integer> a = new ArrayList<Integer>();
-    static Vector<Integer> v = new Vector<Integer>();
+    static int W, H;
+    static int[][] map = new int[51][51];
 
-    public static int DFS(int row, int col, int groupN, int cnt) {
+    public static void DFS(int row, int col) {
 
-        cnt++;
-        visited[row][col] = true;
+        map[row][col] = 0;
 
-        for (int i=0; i<4; i++) {
+        for (int i=0; i<8; i++) {
             int nextRow = row + dRow[i];
             int nextCol = col + dCol[i];
-            // 아래, 왼쪽, 위에, 오른쪽 순으로 탐색
+            // 상, 좌, 하, 우, 왼쪽아래대각선, 오른쪽위대각선, 왼쪽위대각, 오른쪽아래대각
 
-            if (nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= N) continue;
-            if (map[nextRow][nextCol] == 0) continue;
-            if (visited[nextRow][nextCol]) continue;
+            if (nextRow < 0 || nextRow >= H || nextCol < 0 || nextCol >= W) continue;
+            if (map[nextRow][nextCol] == 1) {
+                DFS(nextRow, nextCol);
+            }
 
-            cnt = DFS(nextRow, nextCol, groupN, cnt);
+
         }
-
-        return cnt;
     }
 
     public static void main(String[] args) throws Exception {
 
         FastScanner fs = new FastScanner(System.in);
 
-        // 입력
+        while (true) {
+            W = fs.nextInt();
+            H = fs.nextInt();
 
-        N = fs.nextInt();
+            if (W == 0 && H == 0) break;
 
-
-        for (int a=0; a<N; a++) {
-            String line = fs.next();
-            char[] splitData = line.toCharArray();
-
-            for (int b=0; b<N; b++) {
-                map[a][b] = splitData[b] - '0';
-            }
-        }
-
-        // 계산
-        int groupN = 0;
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<N; j++) {
-                if (!visited[i][j] && map[i][j] != 0) {
-                    // 요소가 있는데 방문을 안한경우 체크
-                    groupN++;
-                    int cnt = DFS(i, j, groupN, 0);
-
-                    v.add(cnt);
+            for (int i=0; i<H; i++) {
+                String line = fs.nextString();
+                String[] lines = line.split(" ");
+                for (int j=0; j<W; j++) {
+                    map[i][j] = Integer.parseInt(lines[j]);
                 }
             }
+
+            int result = 0;
+
+            for (int i=0; i<H; i++) {
+                for (int j=0; j<W; j++) {
+                    if (map[i][j] == 1) {
+                        DFS(i, j);
+                        result++;
+                    }
+                }
+            }
+
+            System.out.println(result);
         }
-
-        Collections.sort(v);
-
-        // 출력
-
-        System.out.println(groupN);
-        for (int data : v) {
-            System.out.println(data);
-        }
-
     }
 }
