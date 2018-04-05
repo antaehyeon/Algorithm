a160d2c1de6bebaadae189a93ad2e465275c02d6
안태현 <antaehyeon@live.co.kr>
2018-04-06 02:24:38 +0900

    [Add] 2007년(1924) JAVA로 풀이 (9368KB, 92MS)

--- Algo(Surface)/src/Main.java
+++ Algo(Surface)/src/Main.java
@@ -33,19 +33,54 @@ class FastScanner {
 
 public class Main {
 
+    final static int MON = 1;
+    final static int TUE = 2;
+    final static int WED = 3;
+    final static int THU = 4;
+    final static int FRI = 5;
+    final static int SAT = 6;
+    final static int SUN = 0;
+
+    static int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     public static void main(String[] args) throws Exception {
 
         FastScanner fs = new FastScanner(System.in);
-        StringBuilder sb = new StringBuilder("");
 
-        int n = fs.nextInt();
+        int month = fs.nextInt();
+        int day = fs.nextInt();
 
-        for (int i=1; i<=9; i++) {
-            sb.append(n + " * " + i + " = " + (n * i) + "\n");
+        int result = day;
+
+        for (int i=1; i<month; i++) {
+            result += days[i-1];
         }
 
-        System.out.println(sb);
+        result %= 7;
 
+        switch (result) {
+            case MON:
+                System.out.println("MON");
+                break;
+            case TUE:
+                System.out.println("TUE");
+                break;
+            case WED:
+                System.out.println("WED");
+                break;
+            case THU:
+                System.out.println("THU");
+                break;
+            case FRI:
+                System.out.println("FRI");
+                break;
+            case SAT:
+                System.out.println("SAT");
+                break;
+            case SUN:
+                System.out.println("SUN");
+                break;
+        }
     }
 }
 
