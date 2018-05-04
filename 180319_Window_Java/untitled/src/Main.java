06306d22d213223f370e6ed3a6ddfe2a67aaa092
HYEON(win) <antaehyeon@live.co.kr>
2018-05-04 13:15:11 +0900

    Feat: 1로 만들기 (1463) JAVA(Bottom-Up)로 풀이 (15000KB, 108MS)

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -14,20 +14,34 @@ public class Main {
 
         d = new int[n+1];
 
-        System.out.println(go(n));
+        System.out.println(bottomUp(n));
     }
 
-    public static int go (int n) {
+    public static int topDown (int n) {
         if (n == 1) return 0;
         if (d[n] > 0) return d[n];
-        d[n] = go(n-1) + 1;
+        d[n] = topDown(n-1) + 1;
         if (n%2 == 0) {
-            int temp = go(n/2) + 1;
+            int temp = topDown(n/2) + 1;
             if (d[n] > temp) d[n] = temp;
         }
         if (n%3 == 0) {
-            int temp = go(n/3) + 1;
+            int temp = topDown(n/3) + 1;
             if (d[n] > temp) d[n] = temp;
+        }
+        return d[n];
+    }
+
+    public static int bottomUp (int n) {
+        d[1] = 0;
+        for (int i=2; i<=n; i++) {
+            d[i] = d[i-1] + 1;
+            if (i%2 == 0 && d[i] > d[i/2] + 1) {
+                d[i] = d[i/2] + 1;
+            }
+            if (i%3 == 0 && d[i] > d[i/3] + 1) {
+                d[i] = d[i/3] + 1;
+            }
         }
         return d[n];
     }
