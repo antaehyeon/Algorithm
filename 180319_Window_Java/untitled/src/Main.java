69d0a926e370373eb2281dbe8750779463aae5b5
HYEON(win) <antaehyeon@live.co.kr>
2018-05-04 18:29:42 +0900

    Feat: 2xn타일링 2 (11727) JAVA(Bottom-up)로 풀이 (11064KB, 88MS)

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -14,14 +14,14 @@ public class Main {
 
         d = new int[n+1];
 
-        System.out.println(topDown(n));
+        System.out.println(bottomUp(n));
     }
 
     public static int topDown (int n) {
         if (n == 0 || n == 1) return 1;
         if (d[n] > 0) return d[n];
 
-        d[n] = topDown(n-2) + topDown(n-1);
+        d[n] = 2 * topDown(n-2) + topDown(n-1);
         d[n] %= 10007;
 
         return d[n];
@@ -31,10 +31,9 @@ public class Main {
         d[0] = 1;
         if (n > 0) d[1] = 1;
         for (int i=2; i<=n; i++) {
-            d[i] = d[i-2] + d[i-1];
+            d[i] = (2 * d[i-2]) + d[i-1];
             d[i] %= 10007;
         }
-        System.out.println(d[n]);
         return d[n];
     }
 }
