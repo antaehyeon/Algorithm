eed87be3c8ea6ec7b4f0e97932d4042453961045
안태현 <antaehyeon@live.co.kr>
2018-05-15 20:29:51 +0900

    Docs: 쉬운 계단 수 (10844) 에 대한 해설내용 추가

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 public class Main {
 
     static long [][] D;
-    static int[] DP;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -13,7 +12,6 @@ public class Main {
         int N = Integer.parseInt(bf.readLine());
 
         D = new long [N+1][10];
-        DP = new int[N+1];
 
 //        System.out.println(topDown(N));
         System.out.println(bottomUp(N));
@@ -34,9 +32,19 @@ public class Main {
             10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98
         */
 
+        // D[1][1...9] = 1
         for (int i=1; i<=9; i++) D[1][i] = 1;
         for (int i=2; i<=n; i++) {
             for (int j=0; j<=9; j++) {
+                /*
+                    D[2][0] 은 1 (D[1][1] = 1)
+                    D[2][1] 은 2 (D[1][0](0) + D[1][2])
+                    D[2][2] 은 2 (D[1][1] + D[1][3])
+                    D[2][3] 은 2 (D[1][2] + D[1][4])
+                    ...
+                    D[2][8] 은 2 (D[1][7] + D[1][9])
+                    D[2][9] 은 1 (D[1][8])
+                */
                 D[i][j] = 0;
                 if (j-1 >= 0) D[i][j] += D[i-1][j-1];
                 if (j+1 <= 9) D[i][j] += D[i-1][j+1];
@@ -49,31 +57,5 @@ public class Main {
         ans %= mod;
 
         return ans;
-
-
-
-//        if (n == 1) {
-//            DP[1] = 9;
-//            return 9;
-//        }
-//
-//
-//        for (int i=1; i<=n; i++) {
-//            DP[i] = 0;
-//            for (int j=0; j<=9; j++) {
-//                D[i][j] = 1;
-//            }
-//            D[i][10] = 0;
-//        }
-//
-//        DP[1] = 9;
-//
-//        for (int i=2; i<=n; i++) {
-//            for (int j=1; j<=9; j++) {
-//                DP[i] += (D[i-1][j-1] + D[i-1][j+1]);
-//            }
-//        }
-//
-//        return DP[n];
     }
 }
