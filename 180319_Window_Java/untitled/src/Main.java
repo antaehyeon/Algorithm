e6192c8e2f8ab8bde34a308ad626b445052e99b9
HYEON(win) <antaehyeon@live.co.kr>
2018-05-20 13:45:11 +0900

    Feat: Codility Lesson 2 Arrays OddOccurrencesInArray JAVA 풀이
    https://app.codility.com/demo/results/trainingAXMGRS-AER/

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -1,42 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        int[] A = {3, 8, 9, 7, 6};
+        int[] A = {9, 3, 9, 3, 9, 7, 9};
 
-        System.out.print(solution(A, 3));
+        System.out.print(solution(A));
 
     }
 
-    public static int[] solution(int[] A, int K) {
+    public static int solution(int[] A) {
         // write your code in Java SE 8
-        int al = A.length;
 
-        if (al == 0) return A;
+        Set<Integer> s = new HashSet<Integer>();
+        int i = A.length;
 
-        K %= al;
-
-        while (K-- > 0) {
-            int temp = 0;
-            int temp2 = 0;
-
-            temp = temp2 = A[1];
-            A[1] = A[0];
-
-            for (int i=1; i<al-1; i++) {
-                temp2 = A[i+1];
-                A[i+1] = temp;
-                temp = temp2;
+        while(i-- > 0) {
+            if (s.contains(A[i])) {
+                s.remove(A[i]);
+            } else {
+                s.add(A[i]);
             }
-
-            A[0] = temp;
         }
 
-        return A;
+        return s.iterator().next();
     }
 }
+
