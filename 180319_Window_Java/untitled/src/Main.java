90ea441838707d36dba5cc80a727bbd166cc7d01
HYEON(win) <antaehyeon@live.co.kr>
2018-05-21 12:38:54 +0900

    Feat: 배민 코테 4번 제출코드

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -1,46 +1,27 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.print(solution("A586QK", "JJ653K"));
-
+        System.out.println(solution(1, 1, 2, 3));
+        System.out.println(solution(2, 4, 2, 4));
+        System.out.println(solution(-5000, 300, 100, 5000));
     }
 
-    public static int solution(String A, String B) {
+    public static int solution(int A, int B, int C, int D) {
         // write your code in Java SE 8
-        int result = 0;
-        String[] ALEC = splitter(A);
-        String[] BOB = splitter(B);
+        int[] points = {A, B, C, D};
+        Arrays.sort(points);
 
-        for (int i=0; i<ALEC.length; i++) {
-            ALEC[i] = transNumeric(ALEC[i]);
-            BOB[i] = transNumeric(BOB[i]);
+        int firstLineLength = points[3] - points[0];
+        int secondLineLength = points[2] - points[1];
 
-            if (stringToNum(ALEC[i]) > stringToNum(BOB[i])) {
-                result++;
-            }
-        }
+        firstLineLength *= firstLineLength;
+        secondLineLength *= secondLineLength;
 
-        return result;
-    }
-
-    public static String[] splitter(String S) {
-        return S.split("");
-    }
-
-    public static String transNumeric(String s) {
-        if (s.equals("A")) return "14";
-        else if (s.equals("K")) return "13";
-        else if (s.equals("Q")) return "12";
-        else if (s.equals("J")) return "11";
-        else if (s.equals("T")) return "10";
-        else return s;
-    }
-
-    public static int stringToNum(String s) {
-        return Integer.parseInt(s);
+        return firstLineLength + secondLineLength;
     }
 }
 
