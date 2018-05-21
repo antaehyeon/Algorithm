b2dde8ca99184f8e0657fb9d022c4f35a66e42c4
HYEON(win) <antaehyeon@live.co.kr>
2018-05-21 12:06:11 +0900

    Feat: 배민 코테 1번 제출코드

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -1,20 +1,83 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 
 public class Main {
+
+    static Stack<Integer> stack = new Stack<>();
+
     public static void main(String[] args) throws IOException {
 
-        System.out.print(solution(10, 85, 30));
+        String s = "3 DUP 5 - -";
+
+        System.out.print(solution(s));
 
     }
 
-    public static int solution(int X, int Y, int D) {
-        // write your code in Java SE 8
-        return ((Y-X) % D > 0) ? ((Y-X) / D + 1) : (Y-X) / D;
+    public static int solution(String S) {
+        boolean exit = false;
+        String[] list = spliter(S);
+
+        for (int i=0; i<list.length; i++) {
+
+            if (exit) return -1;
+
+            if (isNumeric(list[i])) {
+                stack.push(Integer.parseInt(list[i]));
+            } else if (list[i].equals("POP")) {
+                stack.pop();
+            } else if (list[i].equals("DUP")) {
+                stack.push(stack.peek());
+            } else if (list[i].equals("+")) {
+                PLUS();
+            } else if (list[i].equals("-")) {
+                exit = MINUS();
+            } else {
+                System.out.println("ELSE _ PLEASE ERROR CHECK");
+            }
+        }
+
+        if (exit) return -1;
+        return stack.peek();
+    }
+
+    public static String[] spliter(String S) {
+        return S.split(" ");
+    }
+
+    public static boolean isNumeric(String s)
+    {
+        return s.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    public static void PLUS() {
+        int a = stack.pop();
+        int b = stack.pop();
+        stack.push(a + b);
+    }
+
+    public static boolean MINUS() {
+
+        if (stack.size() < 2) {
+            return true;
+        }
+
+        int tmp = stack.pop();
+        int peek = stack.peek();
+
+        if (tmp-peek > 0) {
+            stack.pop();
+            stack.push(tmp-peek);
+        } else {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void exit() {
+        System.out.println(-1);
+        System.exit(-1);
     }
 }
 
