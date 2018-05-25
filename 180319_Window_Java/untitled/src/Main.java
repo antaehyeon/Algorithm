43b827f1b8a14a7fda18fea3574e6d5722c75bf7
안태현 <antaehyeon@live.co.kr>
2018-05-25 23:39:22 +0900

    Feat: 배민 연습 2번문제 풀이 (FastScanner 추가)

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -1,17 +1,45 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
+
+class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    FastScanner(InputStream i) throws Exception {
+        br = new BufferedReader(new InputStreamReader(i));
+    }
+
+    String next() throws Exception {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() throws Exception {
+        return Integer.parseInt(next());
+    }
+}
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+
+        int n = fs.nextInt();
+
+        System.out.println(solution(n));
     }
 
-    public static int solution(int[] A, int N) {
-
+    public static String solution(int N) {
+        return Integer.toHexString(N);
     }
 }
 
