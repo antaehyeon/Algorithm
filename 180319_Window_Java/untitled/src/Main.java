283efa6c103816d30151b3aa272767487354eec5
안태현 <antaehyeon@live.co.kr>
2018-05-28 22:23:55 +0900

    Feat: 배민 2차 코테 3번 문제 제출코드
    DFS 이용

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -27,54 +27,83 @@ class FastScanner {
 
 public class Main {
 
+    static final int[] dRow = {0, -1, 0, 1, -1, 1, -1, 1};
+    static final int[] dCol = {-1, 0, 1, 0, 1, 1, -1, -1};
+
+    static int W, H;
+    static int [][] map = new int[10001][10001];
+
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
         ArrayList<Integer> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
 
-        int x = 0, y = 0;
-        int n = fs.nextInt();
-        int d = fs.nextInt();
-        int k = fs.nextInt();
-        int j = fs.nextInt();
+        int x1 = fs.nextInt();
+        int y1 = fs.nextInt();
+        int x2 = fs.nextInt();
+        int y2 = fs.nextInt();
+        int x3 = fs.nextInt();
+        int y3 = fs.nextInt();
+        int x4 = fs.nextInt();
+        int y4 = fs.nextInt();
+        int x5 = fs.nextInt();
+        int y5 = fs.nextInt();
+        int x6 = fs.nextInt();
+        int y6 = fs.nextInt();
 
-        System.out.println(solution(n, d, k, j));
+        System.out.println(solution(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6));
     }
 
-    public static int solution(int n, int d, int k, int j) {
+    public static int solution(int x1, int y1,int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5, int x6, int y6) {
 
-        LinkedList<Integer> list = new LinkedList<>();
+        int result = 0;
 
-        if (d==1) {
-            for (int i=1; i<=n; i++) {
-                list.add(i);
-            }
-        } else {
-            list.add(1);
-            n += 1;
-            while (n-- > 2) {
-                list.add(n);
+        for (int i=y1; i<y2; i++) {
+            for (int j=x1; j<x2; j++) {
+                map[i][j] = 1;
             }
         }
 
-        int movePosition = k;
-        int index = movePosition;
-        list.remove(index);
-        index -= 1;
-
-        while(list.size() != 1) {
-            movePosition += j;
-            index += movePosition;
-
-            if (index >= list.size()) {
-                index %= list.size();
+        for (int i=y3; i<y4; i++) {
+            for (int j=x3; j<x4; j++) {
+                map[i][j] = 2;
             }
-
-            list.remove(index);
-            index -= 1;
         }
 
-        return list.getFirst();
+        for (int i=y5; i<y6; i++) {
+            for (int j=x5; j<x6; j++) {
+                map[i][j] = 3;
+            }
+        }
+
+        for (int i=y1; i<y2; i++) {
+            for (int j=x1; j<x2; j++) {
+                if (map[i][j] == 1) {
+                    result += DFS(i, j);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static int DFS(int row, int col) {
+
+        int result = 1;
+
+        map[row][col] = -1;
+
+        for (int i = 0; i < 8; i++) {
+            int nextRow = row + dRow[i];
+            int nextCol = col + dCol[i];
+
+            if (nextRow < 0 || nextRow >= H || nextCol < 0 || nextCol >= W) continue;
+            if (map[nextRow][nextCol] == 1) {
+                result += DFS(nextRow, nextCol);
+            }
+        }
+
+        return result;
     }
 }
 
