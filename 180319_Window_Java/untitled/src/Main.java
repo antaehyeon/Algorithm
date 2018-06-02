d69be5e212dc63c3937a9e3943f7801ad33bc7f2
HYEON(win) <antaehyeon@live.co.kr>
2018-06-02 21:58:40 +0900

    Feat: 포도주 시식 (2156) JAVA (Bottom-up) 로 풀이 (11460KB, 104MS)

--- 180319_Window_Java/untitled/src/Main.java
+++ 180319_Window_Java/untitled/src/Main.java
@@ -27,83 +27,32 @@ class FastScanner {
 
 public class Main {
 
-    static final int[] dRow = {0, -1, 0, 1, -1, 1, -1, 1};
-    static final int[] dCol = {-1, 0, 1, 0, 1, 1, -1, -1};
-
-    static int W, H;
-    static int [][] map = new int[10001][10001];
+    static int[] a = new int[10003];
+    static int[] dp = new int[10003];
 
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
 
-        int x1 = fs.nextInt();
-        int y1 = fs.nextInt();
-        int x2 = fs.nextInt();
-        int y2 = fs.nextInt();
-        int x3 = fs.nextInt();
-        int y3 = fs.nextInt();
-        int x4 = fs.nextInt();
-        int y4 = fs.nextInt();
-        int x5 = fs.nextInt();
-        int y5 = fs.nextInt();
-        int x6 = fs.nextInt();
-        int y6 = fs.nextInt();
+        int n = fs.nextInt();
 
-        System.out.println(solution(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6));
-    }
+        for (int i=1; i<=n; i++) {
+            a[i] = fs.nextInt();
+        }
 
-    public static int solution(int x1, int y1,int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5, int x6, int y6) {
+        dp[1] = a[1];
+        dp[2] = a[1] + a[2];
 
-        int result = 0;
-
-        for (int i=y1; i<y2; i++) {
-            for (int j=x1; j<x2; j++) {
-                map[i][j] = 1;
+        for (int i=3; i<=n; i++) {
+            dp[i] = dp[i-1];
+            if (dp[i] < dp[i-2] + a[i]) {
+                dp[i] = dp[i-2] + a[i];
+            }
+            if (dp[i] < dp[i-3] + a[i] + a[i-1]) {
+                dp[i] = dp[i-3] + a[i] + a[i-1];
             }
         }
 
-        for (int i=y3; i<y4; i++) {
-            for (int j=x3; j<x4; j++) {
-                map[i][j] = 2;
-            }
-        }
+        System.out.println(dp[n]);
 
-        for (int i=y5; i<y6; i++) {
-            for (int j=x5; j<x6; j++) {
-                map[i][j] = 3;
-            }
-        }
-
-        for (int i=y1; i<y2; i++) {
-            for (int j=x1; j<x2; j++) {
-                if (map[i][j] == 1) {
-                    result += DFS(i, j);
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public static int DFS(int row, int col) {
-
-        int result = 1;
-
-        map[row][col] = -1;
-
-        for (int i = 0; i < 8; i++) {
-            int nextRow = row + dRow[i];
-            int nextCol = col + dCol[i];
-
-            if (nextRow < 0 || nextRow >= H || nextCol < 0 || nextCol >= W) continue;
-            if (map[nextRow][nextCol] == 1) {
-                result += DFS(nextRow, nextCol);
-            }
-        }
-
-        return result;
     }
 }
-
