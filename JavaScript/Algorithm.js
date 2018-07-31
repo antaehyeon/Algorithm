0316893a5531ec0875caab95aadda6f03e1e10ad
HYEON(mac) <antaehyeon@live.co.kr>
2018-07-31 22:15:51 +0900

    Feat: Codility Lesson 01 Iterations BinaryGap JavaScript (40/100)

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,38 +1,15 @@
-/* Codility Lesson 7 Stacks and Queues 'Fish'
-https://app.codility.com/programmers/lessons/7-stacks_and_queues/fish/ */
+/* Lesson 01 Iterations
+https://app.codility.com/programmers/lessons/1-iterations/ */
 
-function solution (A, B) {
-    
-    let aliveFishNum = A.length;
-    const fishNum = A.length;
-    const bottomFishes = [];
-
-    const peek = function(stack) {
-        return stack.slice(-1)[0];
-    }
-
-    for (let i=0; i<fishNum; i++) {
-
-        // 하류로 이동하는 물고기들을 스택에 담기
-        if (B[i] == 1) {
-            bottomFishes.push(A[i]);
-            continue;
-        }
-
-        while (bottomFishes.length) {
-            aliveFishNum--;
-            if (peek(bottomFishes) > A[i]) {
-                break;
-            } else {
-                bottomFishes.pop();
-            }
-        }
-    }
-
-    return aliveFishNum;
+function solution(N) {
+    let a = N.toString(2).match(/1+0+1/);
+    if (a === null) return 0;
+    return a[0].length-2;
 }
 
-const A = [4, 3, 2, 1, 5];
-const B = [0, 1, 0, 0, 0];
-
-console.log(solution(A, B));
+console.log(solution(9));
+console.log(solution(15));
+console.log(solution(529));
+console.log(solution(20));
+console.log(solution(32));
+console.log(solution(1041));
