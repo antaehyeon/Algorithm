d9d7a1f6ce6cb5c844f9068a08848a246051f38a
HYEON(mac) <antaehyeon@live.co.kr>
2018-07-31 22:48:50 +0900

    Feat: Codility Lesson 01 Iterations BinaryGap JavaScript (60/100)
    https://app.codility.com/demo/results/trainingUKVSGG-JBD/

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -2,14 +2,34 @@
 https://app.codility.com/programmers/lessons/1-iterations/ */
 
 function solution(N) {
-    let a = N.toString(2).match(/1+0+1/);
-    if (a === null) return 0;
-    return a[0].length-2;
+    let max = 0;
+    let s = N.toString(2);
+    let a = s.match(/1+0+1/);
+    let c = s;
+
+    while(true) {
+
+        if (a === null) return 0;
+
+        let b = c.match(/1+0+1/);
+
+        if (b === null) break;
+        else {
+            let temp = b[0].length-2;
+            max = (temp > max) ? temp : max;
+        }
+
+        c = c.replace(/1+0+/, '');
+    }
+
+    return max;
 }
 
-console.log(solution(9));
-console.log(solution(15));
-console.log(solution(529));
-console.log(solution(20));
-console.log(solution(32));
-console.log(solution(1041));
+// console.log(solution(9));
+// console.log(solution(15));
+// console.log(solution(529));
+// console.log(solution(20));
+// console.log(solution(32));
+// console.log(solution(1041));
+// console.log(solution(1162));
+console.log(solution(51712));
