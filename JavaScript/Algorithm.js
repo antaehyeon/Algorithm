8ea156bbf878b5e6c4881166488c9207dd4c25b9
HYEON(mac) <antaehyeon@live.co.kr>
2018-08-02 15:56:02 +0900

    Feat: Codility Lesson 03 Time Complexity TapeEquilibrium JavaScript (100/100)
    https://app.codility.com/demo/results/trainingG2BCAB-UNZ/

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,13 +1,26 @@
 function solution(A) {
     let arrayLen = A.length;
-    let newArr = Array.apply(null, new Array(arrayLen+1)).map(Number.prototype.valueOf, 0);
+    const arrayDataSum = A.reduceRight((a, b) => {return a+b;});
+    let arrayDataSumByIndex = 0;
+    const calculatedDataArr = [];
 
-    A.forEach(element => {
-        newArr[element-1] = 1;
+    let leftSum = 0;
+    let rightSum = arrayDataSum;
+
+    for (let i=1; i<arrayLen; i++) {
+        leftSum += A[i-1];
+        rightSum -= A[i-1];
+        const temp = Math.abs(leftSum - rightSum);
+        calculatedDataArr.push(temp);
+    }
+
+    let minData = calculatedDataArr.reduce((previous, current) => {
+        return previous > current ? current : previous;
     });
 
-    return newArr.indexOf(0) + 1;
-
+    return minData;
 }
 
-console.log(solution([2,3,1,5]));
+console.log(solution([3,1,2,4,3]));
+console.log(solution([0,1,2,-5,2]));
+console.log(solution([-1, 1]));
