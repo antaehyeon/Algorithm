0249805f35b673250ddb365315ae373f4110538a
HYEON(mac) <antaehyeon@live.co.kr>
2018-08-04 23:36:52 +0900

    Feat: Codility Lesson 04 Counting Elements PermCheck JavaScript (100/100)
    https://app.codility.com/demo/results/trainingZPHEFQ-KUH/

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,16 +1,34 @@
+// function solution(A) {
+//     let count = 1;
+//     let arrayLen = A.length;
+
+//     A.sort();
+
+//     for (let i=0; i<arrayLen; i++) {
+//         if (count !== A[i]) return 0;
+//         count++;
+//     }
+
+//     return 1;
+// }
+
 function solution(A) {
+    var arrayLen = A.length;
+    var sum = (arrayLen * (arrayLen+1)) / 2;
+    var tap = [];
 
-    let count = 1;
-
-    A.sort();
-    
-    for (let element of A) {
-        if (element !== count) return 0;
-        count++;
+    for (let i in A) {
+        sum -= A[i];
+        if(tap[A[i]]) {
+            return 0;
+        }
+        tap[A[i]] = true;
     }
-
-    return 1;
+    return +(sum==0);
 }
+
 
 console.log(solution([4,1,3,2]));
 console.log(solution([4,1,3]));
+console.log(solution([2,3,4,5,6,7,8,9]));
+console.log(solution([1,2,2,3,4,5]));
