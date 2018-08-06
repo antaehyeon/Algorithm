035922714d4b3b4de16499a3a23fa33c251f69d1
HYEON(mac) <antaehyeon@live.co.kr>
2018-08-06 20:00:28 +0900

    Feat: Codility Lesson 04 Counting Elements FrogRiverOne JavaScript (18/100)
    https://app.codility.com/demo/results/trainingDKDR66-AK3/

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -12,23 +12,11 @@
 //     return 1;
 // }
 
-function solution(A) {
-    var arrayLen = A.length;
-    var sum = (arrayLen * (arrayLen+1)) / 2;
-    var tap = [];
+function solution(X, A) {
 
-    for (let i in A) {
-        sum -= A[i];
-        if(tap[A[i]]) {
-            return 0;
-        }
-        tap[A[i]] = true;
-    }
-    return +(sum==0);
+    return A.indexOf(X);
+
 }
 
 
-console.log(solution([4,1,3,2]));
-console.log(solution([4,1,3]));
-console.log(solution([2,3,4,5,6,7,8,9]));
-console.log(solution([1,2,2,3,4,5]));
+console.log(solution(5, [1,3,1,4,2,3,5,4]));
