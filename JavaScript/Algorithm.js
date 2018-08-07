a7cb16d6a230d084b67919ce88c61d8435a2e31b
HYEON(mac) <antaehyeon@live.co.kr>
2018-08-08 00:58:03 +0900

    Feat: Codility Lesson 05 Prefix Sums CountDiv JavaScript (50/100)
    https://app.codility.com/demo/results/trainingK7AEC2-7MY/

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,16 +1,12 @@
-function solution(A) {
+function solution(A, B, K) {
 
-    const AL = A.length;
     let COUNT = 0;
-    let PLUS = 0;
 
-    for (I = 0; I < AL; I++) {
-        if (A[I] === 0) PLUS++;
-        else COUNT += PLUS;
-        if (COUNT > 1000000000) return -1;
+    for (I=A; I<=B; I++) {
+        if (I % K === 0) COUNT++;
     }
-    
+
     return COUNT;
 }
 
-console.log(solution([0,1,0,1,1]));
+console.log(solution(6, 11, 2));
