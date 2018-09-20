dc0eb5376e37bec86ce1e92ce08989d03831553c
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-09-21 02:34:37 +0900

    Feat: 백준 골드바흐의 추측 (6588) JAVA (31460KB, 676MS)
    https://www.acmicpc.net/problem/6588

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -24,43 +24,25 @@ class FastScanner {
 }
 
 class Solution {
-    public String solution(int N) {
-        boolean[] isPrimeNumber = new boolean[N+1];
-        ArrayList<Integer> primeNumList = new ArrayList<>();
+    public String solution(boolean[] isPrimeNum, int N) {
 
-        int nSqrt = (int)Math.sqrt(N);
-        int nMax = 0;
         String answer = "";
+        boolean flag = false;
 
-        // 에라토스테네스의 체 코드
-        for (int i=2; i<=nSqrt; i++) {
-            if (isPrimeNumber[i]) continue;
-            for (int j=i+i; j<=N; j+=i) {
-                isPrimeNumber[j] = true;
+        for (int i=2; i<=N/2; i++) {
+            int A, B;
+
+            if (isPrimeNum[i] && isPrimeNum[N-i]) {
+                A = i;
+                B = N-i;
+                flag = true;
+
+                answer = N + " = " + A + " + " + B;
+                break;
             }
         }
 
-        // ArrayList 에 따로 소수만 담는 과정
-        for (int i=2; i<=N; i++) {
-            if (!isPrimeNumber[i]) primeNumList.add(i);
-        }
-
-        // 골드바흐의 추측 알고리즘 (6588)
-        for (int i=0; i<primeNumList.size(); i++) {
-            int nPrime = primeNumList.get(i);
-            int result = N - nPrime;
-            int nTemp = 0;
-
-            if (primeNumList.contains(result))
-                nTemp = result - nPrime;
-
-            if (nTemp > nMax) {
-                nMax = nTemp;
-                answer = N + " = " + nPrime + " + " + result;
-            }
-        }
-
-        if (nMax == 0) return "Goldbach's conjecture is wrong.";
+        if (!flag) return "Goldbach's conjecture is wrong.";
 
         return answer;
     }
@@ -68,16 +50,32 @@ class Solution {
 
 
 public class Main {
+    public static final int MAX = 1000000;
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
         Solution answer = new Solution();
+
+        boolean[] isPrimeNum = new boolean[MAX+1];
+        int nSqrt = (int)Math.sqrt(MAX);
+
+        // 배열 초기화 (false >> true)
+        for (int i=2; i<=MAX; i++)
+            isPrimeNum[i] = true;
+
+        // 에라토스테네스의 체 코드
+        for (int i=2; i<=nSqrt; i++) {
+            if (!isPrimeNum[i]) continue;
+            for (int j=i+i; j<=MAX; j+=i) {
+                isPrimeNum[j] = false;
+            }
+        }
 
         while (true) {
             int n = fs.nextInt();
 
             if (n == 0) break;
 
-            String result = answer.solution(n);
+            String result = answer.solution(isPrimeNum, n);
             System.out.println(result);
         }
     }
