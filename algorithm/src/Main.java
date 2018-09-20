7f7406851b70105a2e33f11799c19c59294137d9
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-09-21 02:07:22 +0900

    Feat: 백준 골드바흐의 추측 (6588) JAVA (시간초과)
    https://www.acmicpc.net/problem/6588

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -24,17 +24,45 @@ class FastScanner {
 }
 
 class Solution {
-    public int checkPrimeNum(int N) {
+    public String solution(int N) {
+        boolean[] isPrimeNumber = new boolean[N+1];
+        ArrayList<Integer> primeNumList = new ArrayList<>();
 
-//        double nSqrt = Math.sqrt(N);
+        int nSqrt = (int)Math.sqrt(N);
+        int nMax = 0;
+        String answer = "";
 
-        if (N == 1) return 0;
-
-        for (int i=2; i<N; i++) {
-            if (N % i == 0) return 0;
+        // 에라토스테네스의 체 코드
+        for (int i=2; i<=nSqrt; i++) {
+            if (isPrimeNumber[i]) continue;
+            for (int j=i+i; j<=N; j+=i) {
+                isPrimeNumber[j] = true;
+            }
         }
 
-        return 1;
+        // ArrayList 에 따로 소수만 담는 과정
+        for (int i=2; i<=N; i++) {
+            if (!isPrimeNumber[i]) primeNumList.add(i);
+        }
+
+        // 골드바흐의 추측 알고리즘 (6588)
+        for (int i=0; i<primeNumList.size(); i++) {
+            int nPrime = primeNumList.get(i);
+            int result = N - nPrime;
+            int nTemp = 0;
+
+            if (primeNumList.contains(result))
+                nTemp = result - nPrime;
+
+            if (nTemp > nMax) {
+                nMax = nTemp;
+                answer = N + " = " + nPrime + " + " + result;
+            }
+        }
+
+        if (nMax == 0) return "Goldbach's conjecture is wrong.";
+
+        return answer;
     }
 }
 
@@ -44,14 +72,13 @@ public class Main {
         FastScanner fs = new FastScanner(System.in);
         Solution answer = new Solution();
 
-        int nRepeat = fs.nextInt();
-        int nPrimeCount = 0;
-
-        for (int i=0; i<nRepeat; i++) {
+        while (true) {
             int n = fs.nextInt();
-            nPrimeCount += answer.checkPrimeNum(n);
-        }
 
-        System.out.println(nPrimeCount);
+            if (n == 0) break;
+
+            String result = answer.solution(n);
+            System.out.println(result);
+        }
     }
 }
