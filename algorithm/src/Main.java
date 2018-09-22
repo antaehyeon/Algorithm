f2f5be0bc5ee7ef81436a25f8a87ea9cf44bd850
HYEON(win) <antaehyeon@live.co.kr>
2018-09-22 17:05:27 +0900

    Feat: 백준 1,2,3 더하기 (9095) JAVA (9888KB, 72MS)
    https://www.acmicpc.net/problem/9095

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -25,86 +25,28 @@ class FastScanner {
 
 public class Main {
 
-    static int N;
-    static int M;
-    static int max;
-
-    static int[][] tetrominoMap;
-    static boolean[][] visited;
-
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static int[] map = new int[11];
 
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
 
-        N = fs.nextInt();
-        M = fs.nextInt();
+        int nRepeat = fs.nextInt();
 
-        tetrominoMap = new int[N][M];
-        visited = new boolean[N][M];
-
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) {
-                tetrominoMap[i][j] = fs.nextInt();
-            }
+        for (int i=0; i<nRepeat; i++) {
+            System.out.println(bottomUp(fs.nextInt()));
         }
 
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<M; j++) {
-                DFS(i, j, 0, 0);
-                Exception(i, j);
-            }
-        }
-
-        System.out.println(max);
     }
 
-    public static void DFS (int x, int y, int depth, int sum) {
-        if (depth == 4) {
-            max = Math.max(max, sum);
-            return;
+    public static int bottomUp(int n) {
+        map[0] = 1; // DP 에서는 0도 1로 인식
+        map[1] = 1; // 1
+        map[2] = 2; // 1+1, 2
+
+        for (int i=3; i<=n; i++) {
+            map[i] = map[i-1] + map[i-2] + map[i-3];
         }
 
-        for (int i=0; i<4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-
-            if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M)
-                continue;
-
-            if (visited[nextX][nextY])
-                continue;
-
-            visited[nextX][nextY] = true;
-            DFS(nextX, nextY, depth+1, sum+tetrominoMap[nextX][nextY]);
-            visited[nextX][nextY] = false;
-        }
-    }
-
-    public static void Exception(int x, int y) {
-        int wing = 4;
-        int min = Integer.MAX_VALUE;
-        int sum = tetrominoMap[x][y];
-
-        for (int i=0; i<4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-
-            if (wing <= 2) return;
-
-            if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
-                wing--;
-                continue;
-            }
-
-            min = Math.min(min, tetrominoMap[nextX][nextY]);
-            sum = sum + tetrominoMap[nextX][nextY];
-        }
-
-        if (wing == 4)
-            sum -= min;
-
-        max = Math.max(max, sum);
+        return map[n];
     }
 }
