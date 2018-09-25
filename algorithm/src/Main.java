8635dcf1f36ec2a8a1c0dbf44e99fb30fdbff844
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-09-25 23:59:04 +0900

    Feat: 백준 외판원 순회 2 (10971) JAVA DFS 풀이 (10720KB, 2352MS)
    https://www.acmicpc.net/problem/10971

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -25,49 +25,50 @@ class FastScanner {
 }
 
 public class Main {
-    static int N;
-    static int max = Integer.MIN_VALUE;
+    static int[][] moveCost;
+    static int[] visit;
+    static int n;
+    static int min = Integer.MAX_VALUE;
     static int cnt = 0;
-    static int repeatNum = 1;
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
 
-        N = fs.nextInt();
-        int[] array = new int[N];
+        n = fs.nextInt();
 
-        for (int i=0; i<N; i++)
-            array[i] = fs.nextInt();
+        moveCost = new int[n][n];
+        visit = new int[n];
 
-        permutation(array, 0);
+        Arrays.fill(visit, 0);
 
-        System.out.println(max);
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                moveCost[i][j] = fs.nextInt();
+            }
+        }
+
+        for (int i=0; i<n; i++)
+            DFS(i, i, 0);
+
+        System.out.print(min);
     }
 
-    public static void permutation(int[] arr, int d) {
-        if (d == N) {
-            cnt++;
+    public static void DFS(int first, int a, int sum) {
+        if (cnt == n && first == a) {
+            min = Math.min(min, sum);
             return;
         }
 
-        for (int i=d; i<N; i++) {
-            swap(arr, d, i);
-            permutation(arr, d+1);
-            maxDifferece(arr);
-            swap(arr, d, i);
+        for (int i=0; i<n; i++) {
+            if (visit[i] == 0 && moveCost[a][i] != 0) {
+                visit[i] = 1;
+                sum += moveCost[a][i];
+                cnt++;
+                DFS(first, i, sum);
+                visit[i] = 0;
+                sum -= moveCost[a][i];
+                cnt--;
+            }
         }
     }
 
-    public static void swap (int[] arr, int d, int i) {
-        int temp = arr[i];
-        arr[i] = arr[d];
-        arr[d] = temp;
-    }
-
-    public static void maxDifferece(int[] array) {
-        int sum = 0;
-        for (int i=0; i<array.length-1; i++) {
-            sum += Math.abs(array[i]-array[i+1]);
-        }
-        max = Math.max(max, sum);
-    }
 }
