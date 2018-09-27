32058c7ed4769270ebf8472ed420677e4e4547e4
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-09-28 06:41:14 +0900

    Feat: 백준 집합 (11723) JAVA (207272KB, 1260MS)
    https://www.acmicpc.net/problem/11723

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -19,6 +19,10 @@ class FastScanner {
         return st.nextToken();
     }
 
+    String nextLine() throws Exception {
+        return br.readLine();
+    }
+
     int nextInt() throws Exception {
         return Integer.parseInt(next());
     }
@@ -26,41 +30,48 @@ class FastScanner {
 
 public class Main {
 
-    static int N;
-    static int[] T;
-    static int[] P;
-    static int maxPay = Integer.MIN_VALUE;
-    static int adviceTime = 0;
+    static StringBuilder sb = new StringBuilder("");
 
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
+        int M = fs.nextInt();
 
-        N = fs.nextInt();
+        int n;
+        int bitSet = 0;
 
-        T = new int[N];
-        P = new int[N];
+        for (int i=0; i<M; i++) {
+            String[] cmd = fs.nextLine().split(" ");
 
-        for (int i=0; i<N; i++) {
-            T[i] = fs.nextInt();
-            P[i] = fs.nextInt();
+            switch (cmd[0]) {
+                case "add":
+                    n = Integer.parseInt(cmd[1]);
+                    bitSet = bitSet | (1<<n);
+                    break;
+                case "remove":
+                    n = Integer.parseInt(cmd[1]);
+                    bitSet = bitSet & ~(1<<n);
+                    break;
+                case "check":
+                    n = Integer.parseInt(cmd[1]);
+                    int result = (bitSet & (1<<n));
+                    if (result > 0)
+                        sb.append("1\n");
+                    else if (result == 0)
+                        sb.append("0\n");
+                    break;
+                case "toggle":
+                    n = Integer.parseInt(cmd[1]);
+                    bitSet = bitSet ^ (1<<n);
+                    break;
+                case "all":
+                    bitSet = (1 << 21) - 1;
+                    bitSet = bitSet & ~(1);
+                    break;
+                case "empty":
+                    bitSet = 0;
+                    break;
+            }
         }
-
-        DFS(0, 0);
-
-        System.out.print(maxPay);
-    }
-
-    public static void DFS(int d, int sum) {
-        if (d == N) {
-            maxPay = Math.max(maxPay, sum);
-            return;
-        }
-
-        if (d+T[d] <= N) {
-            DFS(d + T[d], sum + P[d]);
-        }
-        if (d+1 <= N) {
-            DFS(d+1, sum);
-        }
+        System.out.println(sb);
     }
 }
