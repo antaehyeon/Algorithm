b31072acc28159daccca7cb1fc4e48337de6b271
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-09-28 05:26:44 +0900

    Feat: 백준 부분집합의 합 (1182) JAVA (10108KB, 84MS)
    https://www.acmicpc.net/problem/1182

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -26,69 +26,40 @@ class FastScanner {
 
 public class Main {
 
-    static StringBuilder sb = new StringBuilder("");
-    static int L;
-    static int C;
-    static char[] alphabets;
+    static int subSetCnt = 0;
+    static int N;
+    static int S;
+    static int[] nums;
     static boolean[] visit;
-
 
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
 
-        L = fs.nextInt();
-        C = fs.nextInt();
+        N = fs.nextInt();
+        S = fs.nextInt();
 
-        alphabets = new char[C];
-        visit = new boolean[26]; // 알파벳 갯수만큼 초기화
+        nums = new int[N];
+        visit = new boolean[N];
 
-        for (int i=0; i<C; i++)
-            alphabets[i] = fs.next().charAt(0);
+        for (int i=0; i<N; i++) {
+            nums[i] = fs.nextInt();
+        }
 
-        Arrays.sort(alphabets);
+        DFS(0, 0, 0);
 
-        for (int i=0; i<=C-1; i++)
-            DFS(i, 1, "" + alphabets[i]);
-
-        System.out.println(sb);
+        System.out.print(subSetCnt);
     }
 
-    public static void DFS(int v, int cnt, String s) {
-        int idx = alphabets[v] - 'a';
-        visit[idx] = true;
-
-        if (L == cnt) {
-            if (isSatisfy())
-                sb.append(s + "\n");
-        } else {
-            for (int i=v+1; i<C; i++) {
-                if (!visit[alphabets[i] - 'a'])
-                    DFS(i, cnt+1, s+alphabets[i]);
+    public static void DFS(int v, int sum, int len) {
+        if (v == N) {
+            if (sum == S && len != 0) {
+                subSetCnt++;
+                return;
             }
+            return;
         }
 
-        // backtracking
-        visit[idx] = false;
+        DFS(v+1, sum + nums[v], len+1);
+        DFS(v+1, sum, len);
     }
-
-    public static boolean isSatisfy() {
-        int vowel = 0; // 모음
-        int consonant = 0; // 자음
-
-        for (int i=0; i<26; i++) {
-            // 모음갯수 판별
-            if (i==0 || i==4 || i==8 || i==14 || i==20) {
-                if (visit[i])
-                    vowel++;
-            // 나머지 자음
-            } else if (visit[i])
-                consonant++;
-        }
-
-        if (vowel > 0 && consonant > 1)
-            return true;
-
-        return false;
-    }
-
 }
