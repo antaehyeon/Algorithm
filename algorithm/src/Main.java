6ec7e990e177714a2511db347d58eb2a7655af20
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-09-28 06:21:52 +0900

    Feat: 백준 퇴사 (14501) JAVA (10192KB, 76MS)
    https://www.acmicpc.net/problem/14501

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -26,40 +26,41 @@ class FastScanner {
 
 public class Main {
 
-    static int subSetCnt = 0;
     static int N;
-    static int S;
-    static int[] nums;
-    static boolean[] visit;
+    static int[] T;
+    static int[] P;
+    static int maxPay = Integer.MIN_VALUE;
+    static int adviceTime = 0;
 
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
 
         N = fs.nextInt();
-        S = fs.nextInt();
 
-        nums = new int[N];
-        visit = new boolean[N];
+        T = new int[N];
+        P = new int[N];
 
         for (int i=0; i<N; i++) {
-            nums[i] = fs.nextInt();
+            T[i] = fs.nextInt();
+            P[i] = fs.nextInt();
         }
 
-        DFS(0, 0, 0);
+        DFS(0, 0);
 
-        System.out.print(subSetCnt);
+        System.out.print(maxPay);
     }
 
-    public static void DFS(int v, int sum, int len) {
-        if (v == N) {
-            if (sum == S && len != 0) {
-                subSetCnt++;
-                return;
-            }
+    public static void DFS(int d, int sum) {
+        if (d == N) {
+            maxPay = Math.max(maxPay, sum);
             return;
         }
 
-        DFS(v+1, sum + nums[v], len+1);
-        DFS(v+1, sum, len);
+        if (d+T[d] <= N) {
+            DFS(d + T[d], sum + P[d]);
+        }
+        if (d+1 <= N) {
+            DFS(d+1, sum);
+        }
     }
 }
