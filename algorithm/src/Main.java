a05749039170f0b95345638f73b9cc1908b88577
HYEON-MACMINI <antaehyeon@live.co.kr>
2018-09-28 23:31:23 +0900

    Feat: 백준 관중석 (1764) JAVA (24688KB, 376MS)
    Binary Search 이용
    
    https://www.acmicpc.net/problem/1764

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -33,63 +33,38 @@ class FastScanner {
 
 public class Main {
 
-    static ArrayList<Integer> landSizes = new ArrayList<>();
-
-    static final int[] dRow = {0, -1, 0, 1, -1, 1, -1, 1};
-    static final int[] dCol = {-1, 0, 1, 0, 1, 1, -1, -1};
-
-    static int W, H;
-    static int[][] map;
-
-    static int landSize = 0;
-
-    public static void DFS(int row, int col) {
-
-        map[row][col] = 0;
-
-        for (int i=0; i<8; i++) {
-            int nextRow = row + dRow[i];
-            int nextCol = col + dCol[i];
-            // 상, 좌, 하, 우, 왼쪽아래대각선, 오른쪽위대각선, 왼쪽위대각, 오른쪽아래대각
-
-            if (nextRow < 0 || nextRow >= H || nextCol < 0 || nextCol >= W) continue;
-            if (map[nextRow][nextCol] == 1) {
-                landSize++;
-                DFS(nextRow, nextCol);
-            }
-        }
-    }
+    static int N, M;
+    static ArrayList<String> list = new ArrayList<String>();
+    static ArrayList<String> answerPeople = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-
         FastScanner fs = new FastScanner(System.in);
 
-        W = H = fs.nextInt();
-        map = new int[W][H];
+        N = fs.nextInt();
+        M = fs.nextInt();
 
-        for (int i=0; i<H; i++) {
-            for (int j=0; j<W; j++) {
-                map[i][j] = fs.nextInt();
-            }
+        for (int i=0; i<N; i++)
+            list.add(fs.next());
+
+        Collections.sort(list);
+        String[] peoples = new String[list.size()];
+        peoples = list.toArray(peoples);
+
+        list.clear();
+
+        for (int i=0; i<M; i++) {
+            String name = fs.next();
+            list.add(name);
+            int idx = Arrays.binarySearch(peoples, name);
+            if (idx >= 0)
+                answerPeople.add(name);
         }
 
-        int result = 0;
+        Collections.sort(answerPeople);
 
-        for (int i=0; i<H; i++) {
-            for (int j=0; j<W; j++) {
-                if (map[i][j] == 1) {
-                    DFS(i, j);
-                    result++;
-                    landSizes.add(landSize+1);
-                    landSize = 0;
-                }
-            }
-        }
+        System.out.println(answerPeople.size());
+        for (String s : answerPeople)
+            System.out.println(s);
 
-        System.out.println(result);
-        Collections.sort(landSizes);
-        for (int size : landSizes) {
-            System.out.print(size + " ");
-        }
     }
 }
