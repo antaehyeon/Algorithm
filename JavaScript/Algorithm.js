ef4192b90f218ad693074aa996383dd29295fe2b
HYEON(Mac) <antaehyeon@live.co.kr>
2019-07-02 12:18:01 +0900

    Feat: 프로그래머스 스킬 체크 테스트 Level 1-1

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,14 +1,23 @@
-const readline=require("readline");
+// 프로그래머스 스킬 체크 테스트 Level 1
+/*
+문제 설명
+함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
 
-const rl=readline.createInterface({
-  input:process.stdin,
-  output:process.stdout
-});
+제한 조건
+x는 -10000000 이상, 10000000 이하인 정수입니다.
+n은 1000 이하인 자연수입니다.
+*/
 
-rl.setPrompt("## ");
+function solution(x, n) {
+  var answer = [];
 
-rl.prompt();
-rl.on("line",(data)=>{
-  console.log(data);
-  rl.prompt();
-});
+  for (let i = 1; i <= n; i++) {
+    answer.push(x * i);
+  }
+
+  return answer;
+}
+
+console.log(solution(2, 5));
+console.log(solution(4, 3));
+console.log(solution(-4, 2));
