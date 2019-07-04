a3e571c1cf0d3c348051fd10d8827f65999a2c07
HYEON(iMac) <antaehyeon@live.co.kr>
2019-07-04 17:14:56 +0900

    Feat: 프로그래머스 탑 JS 풀이

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,62 +1,28 @@
 /*
-프로그래머스 - 다리를 지나는 트럭
+프로그래머스 - 탑
 */
 
-class Queue {
-  constructor() {
-    this._arr = [];
-  }
-  enqueue(item) {
-    this._arr.push(item);
-  }
-  dequeue() {
-    return this._arr.shift();
-  }
-  sum() {
-    let sum = 0;
-    for (const data of this._arr) sum += data;
+function solution(heights) {
+  var answer = [];
+  const topNumbers = heights.length;
 
-    return sum;
+  heights.reverse();
+
+  for (const topIdx in heights) {
+    for (let i = parseInt(topIdx) + 1; i <= topNumbers; i++) {
+      if (heights[topIdx] < heights[i]) {
+        answer.push(topNumbers - i);
+        break;
+      }
+      if (i === topNumbers) {
+        answer.push(0);
+      }
+    }
   }
-  count() {
-    return this._arr.length;
-  }
+
+  answer.reverse();
+
+  return answer;
 }
 
-function solution(bridge_length, weight, truck_weights) {
-  var time = 0;
-  console.log(time);
-
-  const isFlag = truck_weights.length % 2 != 0;
-
-  const bridge = new Queue();
-
-  while (true) {
-    if (time === 98) {
-      // console.log("98");
-    }
-
-    if (time % bridge_length === 0) {
-      bridge.dequeue();
-    }
-    if (truck_weights.length === 0 && bridge.count() === 0) break;
-    time++;
-
-    if (truck_weights.length === 0) continue;
-
-    const truckWeight = truck_weights.shift();
-
-    if (bridge.sum() + truckWeight > weight) {
-      truck_weights.unshift(truckWeight);
-      continue;
-    }
-
-    bridge.enqueue(truckWeight);
-  }
-
-  return time + isFlag;
-}
-
-console.log(solution(2, 10, [7, 4, 5, 6]));
-console.log(solution(100, 100, [10]));
-console.log(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]));
+console.log(solution([6, 9, 5, 7, 4]));
