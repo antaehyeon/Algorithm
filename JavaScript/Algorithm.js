58b19e201e45d6acc13ec521eadaa98577c65ad7
Hyeon <antaehyeon@live.co.kr>
2019-07-05 12:55:12 +0900

    Feat: 부스트캠프 2019 데모테스트 1 JS 코드

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,7 +1,8 @@
 /*
-프로그래머스 - 프린터
+프로그래머스 - 부스트캠프 2019 데모테스트 1
 */
 
+<<<<<<< Updated upstream
 // 정확성 50 효율성 0
 // function solution(participant, completion) {
 //   var answer = "";
@@ -42,3 +43,36 @@ const participant = ["mislav", "stanko", "mislav", "ana"];
 const completion = ["stanko", "ana", "mislav"];
 
 console.log(solution(participant, completion));
+=======
+const X = 0;
+const Y = 1;
+
+const ONE = 1;
+
+function solution(v) {
+  const answer = [];
+
+  const x = [];
+  const y = [];
+
+  v.some(lo => {
+    console.log(x.includes(lo[X]));
+    console.log(y.includes(lo[Y]));
+
+    if (x.includes(lo[X])) x.splice(x.indexOf(lo[X]), ONE);
+    else x.push(lo[X]);
+
+    if (y.includes(lo[Y])) y.splice(y.indexOf(lo[Y]), ONE);
+    else y.push(lo[Y]);
+  });
+
+  console.log(x);
+  console.log(y);
+  // console.log(x.concat(y));
+
+  return x.concat(y);
+}
+const v = [[1, 4], [3, 4], [3, 10]];
+
+console.log(solution(v));
+>>>>>>> Stashed changes
