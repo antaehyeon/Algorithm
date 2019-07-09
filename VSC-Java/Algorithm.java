cafbead4db4f54b4433af42abdfd5c9447866548
Hyeon <antaehyeon@live.co.kr>
2019-07-09 12:54:18 +0900

    Feat: 프로그래머스 해시 전화번호 목록 JS, Java 풀이

--- VSC-Java/Algorithm.java
+++ VSC-Java/Algorithm.java
@@ -4,33 +4,29 @@ import java.util.*;
 public class Algorithm
 {
 	public static void main(String[] args) {
-		int[] A = {4, 3, 2, 1, 5};
-		int[] B = {0, 1, 0, 0, 0};
+        String[] A = {"119", "97674223", "1195524421"};
+        String[] B = {"123","456","789"};
+        String[] C = {"12","123","1235","567","88"};
 
-		System.out.println(solution(A, B));
+		System.out.println(solution(A));
+		System.out.println(solution(B));
+		System.out.println(solution(C));
 	}
 
-    public static int solution(int[] A, int[] B) {
+    public static boolean solution(String[] phone_book) {
+        boolean answer = false;
 
-        int aliveFishNum = A.length;
-        int fishNum = A.length;
-        Stack<Integer> bottomFishes = new Stack<>();
+        Arrays.sort(phone_book);
 
-        for (int i=0; i<fishNum; i++) {
-            if (B[i] == 1) {
-                bottomFishes.push(A[i]);
-                continue;
-            }
-
-            while (bottomFishes.size() != 0) {
-                aliveFishNum--;
-                if (bottomFishes.peek() > A[i]) {
-                    break;
-                } else {
-                    bottomFishes.pop();
-                }
+        for (int i=0; i<phone_book.length-1; i++) {
+            for (int j=i+1; j<phone_book.length; j++) {
+                if (phone_book[j].startsWith(phone_book[i])) return answer;
+                if (phone_book[i].startsWith(phone_book[j])) return answer;
             }
         }
-        return aliveFishNum;
+        
+        answer = true;
+
+        return answer;
     }
 }
