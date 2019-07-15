53d67eed2d63226cf5e1ae7cb2b0c14f1bad2646
Hyeon <antaehyeon@live.co.kr>
2019-07-15 23:40:09 +0900

    Feat: 백준 최대공약수와 최소공배수(2609) Python3 풀이 https://www.acmicpc.net/problem/2609

--- Python/algorithm.py
+++ Python/algorithm.py
@@ -1,34 +1,13 @@
-answer = 0
-n = int(input())
+def gcd(a, b):
+    if b == 0: return a
+    else: return gcd(b, a % b)
+
+
+# 2개의 입력값이 정해져 있다면 n, m = map(int, input().split())
 L = list(map(int, input().split()))
+a = L[0]
+b = L[1]
+gcdN = gcd(a, b)
 
-
-def prime(p):
-    if (p < 2): return False
-    i = 2
-    while i * i <= p:
-        if (p % i == 0): return False
-        i += 1
-
-    return True
-
-
-for i in L:
-    if (prime(i)): answer += 1
-
-print(answer)
-
-
-def solution(numbers, target):
-    answer_list = [0]
-    for i in numbers:
-        temporary_list = []
-        for j in answer_list:
-            temporary_list.append(j + i)
-            temporary_list.append(j - i)
-        answer_list = temporary_list
-    answer = answer_list.count(target)
-    return answer
-
-
-print(solution([1, 1, 1, 1, 1], 3))
+print(gcdN)
+print(int((a / gcdN) * b))
