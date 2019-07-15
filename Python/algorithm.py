58e8709512927048a1dc583399a09983de77b530
Hyeon <antaehyeon@live.co.kr>
2019-07-15 23:55:23 +0900

    Feat: 백준 최소공배수(1934) Python3 풀이 https://www.acmicpc.net/problem/1934

--- Python/algorithm.py
+++ Python/algorithm.py
@@ -3,11 +3,7 @@ def gcd(a, b):
     else: return gcd(b, a % b)
 
 
-# 2개의 입력값이 정해져 있다면 n, m = map(int, input().split())
-L = list(map(int, input().split()))
-a = L[0]
-b = L[1]
-gcdN = gcd(a, b)
-
-print(gcdN)
-print(int((a / gcdN) * b))
+r = int(input())
+for i in range(r):
+    a, b = map(int, input().split())
+    print(int(a / gcd(a, b) * b))
