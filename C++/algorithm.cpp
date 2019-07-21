726ae9e3456b994f7361238d66e3d21a82b86faa
HYEON(iMac) <antaehyeon@live.co.kr>
2019-07-22 05:32:59 +0900

    Feat: 백준 연산자 풀어넣기 (14888) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -5,31 +5,50 @@
 #include <climits>
 using namespace std;
 
+int cal(int m, int a, int b) {
+    if (m == 0) return a+b;
+    else if (m == 1) return a-b;
+    else if (m == 2) return a*b;
+    else return a/b;
+}
+
 int main() {
+
+    int max = INT_MIN;
+    int min = INT_MAX;
+
     int N;
-    while (N != 0) {
-        vector<int> a;
+    cin >> N;
 
-        cin >> N;
+    int A[N];
+    for (int i=0; i<N; i++) cin >> A[i];
 
-        for (int i=0; i<N; i++) {
-            if (i >= 6) a.push_back(0);
-            else a.push_back(1);
+    vector<int> B;
+    for (int i=0; i<4; i++) {
+        int t;
+        cin >> t;
+
+        if (t != 0) {
+            for (int j=0; j<t; j++) B.push_back(i);
+        }
+    }
+    
+    do {
+        int res = 0;
+        int prev = A[0];
+        for (int i=0; i<N-1; i++) {
+            res = cal(B[i], prev, A[i+1]);
+            cout << "res : " << res << " prev: " << prev << endl;
+            prev = res;
         }
 
-        vector<int> v(N);
+        if (res > max) max = res;
+        if (res < min) min = res;
 
-        for (int i=0; i<N; i++) cin >> v[i];
+        cout << "** max : " << max << " min : " << min << endl; 
+    } while (next_permutation(B.begin(), B.end()));
 
-        do {
-            for (int i=0; i<N; i++) {
-                if (a[i] == 1) {
-                        cout << v[i] << " ";
-                    }
-                }
-            cout << "\n";
-        } while (prev_permutation(a.begin(), a.end()));
+    cout << max << "\n";
+    cout << min;
 
-        cout << "\n";
-    }
 }
