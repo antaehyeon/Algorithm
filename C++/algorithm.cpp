36b0ccde9a0d69c7e6dc9ab6752bea6eb4b317f9
HYEON(iMac) <antaehyeon@live.co.kr>
2019-07-22 00:27:58 +0900

    Feat: 백준 차이를 최대로 (10819) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cstdlib>
 using namespace std;
 
+int cal(vector<int> v) {    
+    int sum = 0;
+    for (int i=0; i<v.size()-1; i++) {
+        sum += (abs(v[i] - v[i+1]));
+    }
+
+    return sum;
+}
+
 int main() {
-    int N;
+    int N, ans = -101;
     cin >> N;
+    vector<int> a(N);
 
-    vector<int> v(N);
+    for (int i=0; i<N; i++) cin >> a[i];
 
-    for (int i=0; i<N; i++) v[i] = i+1;
+    sort(a.begin(), a.end());
 
     do {
-        for (auto i : v) cout << i << " ";
-        cout << "\n";
-    } while (next_permutation(v.begin(), v.end()));
+        int temp = cal(a);
+        ans = max(ans, temp);
+    } while (next_permutation(a.begin(), a.end()));
 
-    return 0;
+    cout << ans;
 }
