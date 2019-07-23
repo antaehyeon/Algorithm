9a4554029e9ac871ec2c790dbc857a91b974e1c5
HYEON(iMac) <antaehyeon@live.co.kr>
2019-07-24 03:45:05 +0900

    Feat: 백준 일곱난쟁이(2309) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -3,38 +3,33 @@
 #include <vector>
 #include <cstdlib>
 #include <climits>
-#define N 1000000
 using namespace std;
 
 
 int main() {
-    int prime[N];
-    int pn = 0;
-    bool check[N+1];
-    for (int i=2; i*i<=N; i++) {
-        if (!check[i]) {
-            prime[pn++] = i;
-            // 이쪽 for문은 N에 따라서 i*i 또는 i*2로 바꾸는 것이 좋다 (i가 백만인 경우 i*i는 범위를 넘어감)
-            for(int j=i*i; j<=N; j+=i) check[j] = true;
-        }
+    vector<int> v;
+    vector<int> s;
+
+    for (int i=0; i<9; i++) {
+        int t;
+        scanf("%d", &t);
+        v.push_back(t);
     }
 
-    while(1) {
-        bool flag = false;
-        int n;
-        scanf("%d", &n);
+    sort(v.begin(), v.end());
 
-        if (n==0) break;
+    do {
+        int sum = 0;
+        for (int i=0; i<7; i++) sum += v[i];
 
-        for(int i : prime) {
-            if (i == 2) continue;
-            if (!check[n-i]) {
-                flag = true;
-                printf("%d = %d + %d\n", n, i, n-i);
-                break;
-            }
+        if (sum == 100) {
+            for (int i=0; i<7; i++) s.push_back(v[i]);
+            break;
         }
+    } while(next_permutation(v.begin(), v.end()));
 
-        if (!flag) printf("Goldbach's conjecture is wrong.");
-    }
+    sort(s.begin(), s.end());
+    
+    for (auto i : s) printf("%d\n", i);
+
 }
