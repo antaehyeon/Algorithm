75f6f250be7e0d2310abbf7bd04d0b2db16ac571
Hyeon <antaehyeon@live.co.kr>
2019-08-04 21:37:07 +0900

    Feat: 미세먼지 안녕!(17144) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,94 +1,146 @@
 #include <iostream>
-#include <queue>
 #include <algorithm>
+#include <queue>
+#include <vector>
+#include <utility>
 #include <string.h>
-#define MAX 9
-
+#define MAX 50
 using namespace std;
 
-int R, C, zeroCnt, maxZero;
-int dR[] = {1, 0, -1, 0};
-int dC[] = {0, 1, 0, -1};
+int R, C, T;
+int arr[MAX][MAX], tmp[MAX][MAX];
+int dY[] = {1, 0, -1, 0};
+int dX[] = {0, 1, 0, -1};
+int cw[4] = {1, 0, 3, 2};
+int ccw[4] = {1, 2, 3, 0};
+queue<pair<int, int>> airQ;
+queue<pair<int, int>> dustQ;
+pair<int, int> firstAirQ;
+pair<int, int> secondAirQ;
 
-int map[MAX][MAX], tmp[MAX][MAX];
-queue<pair<int, int>> baseQ;
-
-bool isInRange(int row, int col) {
-    return (row >=0 && row < R) && (col >= 0 && col < C);
+int isInArrRange(int X, int Y) {
+    return (0 <= X && X < C) && (0 <= Y && Y < R);
 }
 
-void BFS(int& zero) {
-    int afterWall[MAX][MAX];
-    memcpy(afterWall, tmp, sizeof(tmp));
-    int zC = zero;
-    queue<pair<int, int>> Q = baseQ;
+void Print() {
+    // cout << "-----------------------" << endl;
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "-----------------------" << endl;
+}
 
-    while(!Q.empty()) {
-        int cR = Q.front().first;
-        int cC = Q.front().second;
-        Q.pop();
+void makeDustQ() {
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
+            if (arr[i][j] >= 1) dustQ.push({i, j});
+        }
+    }
+}
 
-        for (int i=0; i<4; ++i) {
-            int nR = cR + dR[i];
-            int nC = cC + dC[i];
+void BFS() {
+    memcpy(tmp, arr, sizeof(arr));
+    while (!dustQ.empty()) {
+        int X = dustQ.front().second;
+        int Y = dustQ.front().first;
+        dustQ.pop();
 
-            if (isInRange(nR, nC) && afterWall[nR][nC] == 0) {
-                afterWall[nR][nC] = 2;
-                --zC;
-                Q.push({nR, nC});
+        int virus = tmp[Y][X] / 5; // 여기에 임시배열을 쓰지 않으면 결과값이 이상하게 출력됨
+        for (int i=0; i<4; i++) {
+            int nextX = X + dX[i];
+            int nextY = Y + dY[i];
+
+            if (isInArrRange(nextX, nextY)) {
+                if (tmp[nextY][nextX] >= 0) { // 여기에 임시배열을 쓰지 않으면 결과값이 이상하게 출력됨
+                    arr[nextY][nextX] += virus;
+                    arr[Y][X] -= virus;
+                }
             }
         }
     }
-    maxZero = max(maxZero, zC);
 }
 
-void makeWall(int cnt, int& zero) {
-    if (cnt == 3) {
-        BFS(zero);
-        return;
+void cleaning() {
+    memcpy(tmp, arr, sizeof(arr));
+    int X = firstAirQ.second+1;
+    int Y = firstAirQ.first;
+    arr[Y][X] = 0;
+
+    for (int i=0; i<4; i++) {
+        // cout << Y << "," << X << endl;
+        while(1) {
+            int nextX = X + dX[ccw[i]];
+            int nextY = Y + dY[ccw[i]];
+            // cout << dX[ccw[i]] << ", " << dY[ccw[i]] << endl;
+            // cout << nextX << ", " << nextY << endl;
+
+            if (nextX == firstAirQ.second && nextY == firstAirQ.first) break;
+            if (!isInArrRange(nextX, nextY)) break;
+
+            arr[nextY][nextX] = tmp[Y][X];
+            X = nextX;
+            Y = nextY;
+        }
     }
-    for (int i=0; i<R; ++i) {
-        for (int j=0; j<C; ++j) {
-            if (tmp[i][j] == 0) {
-                tmp[i][j] = 1;
-                --zero;
-                makeWall(cnt + 1, zero);
-                tmp[i][j] = 0;
-                ++zero;
-            }
+}
+
+void cleaning2() {
+    memcpy(tmp, arr, sizeof(arr));
+    int X = secondAirQ.second+1;
+    int Y = secondAirQ.first;
+    arr[Y][X] = 0;
+
+    for (int i=0; i<4; i++) {
+        while(1) {
+            int nextX = X + dX[cw[i]];
+            int nextY = Y + dY[cw[i]];
+
+            if (nextX == secondAirQ.second && nextY == secondAirQ.first) break;
+            if (!isInArrRange(nextX, nextY)) break;
+
+            arr[nextY][nextX] = tmp[Y][X];
+            Y = nextY;
+            X = nextX;
         }
     }
 }
 
 int main () {
-    cin >> R >> C;
-    zeroCnt = R * C;
+    int result = 0;
+    cin >> R >> C >> T;
 
-    // 바이러스 좌표 찾기
-    for (int i=0; i<R; ++i) {
-        for (int j=0; j<C; ++j) {
-            cin >> map[i][j];
-
-            if (map[i][j] != 0) {
-                --zeroCnt;
-                if (map[i][j] == 2) baseQ.push({i, j});
-            }
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
+            cin >> arr[i][j];
+            // 만약 공기청정기의 좌표라면 보관
+            if (arr[i][j] == -1) airQ.push({i, j});
         }
     }
 
-    for (int i=0; i<R; ++i) {
-        for (int j=0; j<C; ++j) {
-            if (map[i][j] == 0) {
-                memcpy(tmp, map, sizeof(map));
-                tmp[i][j] = 1;
-                --zeroCnt;
-                makeWall(1, zeroCnt);
-                tmp[i][j] = 0;
-                ++zeroCnt;
-            }
+    firstAirQ = airQ.front();
+    airQ.pop();
+    secondAirQ = airQ.front();
+    airQ.pop();
+
+    for (int i=0; i<T; i++) {
+        // cout << i << "번 째 --------------------" <<endl;
+        // Print();
+        makeDustQ();
+        BFS(); // 미세먼지 확산을 동시에 일으킴
+        cleaning();
+        cleaning2();
+    }
+
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
+            if (arr[i][j] >= 1) result += arr[i][j];
         }
     }
 
-    cout << maxZero << endl;
-    
+    cout << result;
+
+    return 0;
 }
