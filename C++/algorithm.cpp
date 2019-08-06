eaf5f520a40e568c4bc1819bb8bafeee77a7f708
Hyeon <antaehyeon@live.co.kr>
2019-08-06 22:14:47 +0900

    Feat: 로봇 청소기(14503) C++ 풀이 (틀림)

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,63 +1,88 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <climits>
+#include <queue>
+#define MAX 51
 
 using namespace std;
+int N, M;
+int r, c, d;
+int currentD;
 
-#define MAX 21
+int arr[MAX][MAX];
+int dX[4] = {0, 1, 0, -1};
+int dY[4] = {-1, 0, 1, 0};
 
-int N, answer = INT_MAX;
-int S[MAX][MAX];
-bool chk[MAX];
+bool isExit = false;
 
-void DFS(int cur, int cnt) {
-    if (cnt == N/2) {
-        vector<int> linkTeam, startTeam;
-        for (int i=0; i<N; i++) {
-            if (chk[i]) startTeam.push_back(i);
-            else linkTeam.push_back(i);
+int calDirection(int p) {
+    if (p == 0) return 3;
+    else if (p == 3) return 2;
+    else if (p == 2) return 1;
+    else return 0;
+}
+
+int backDirection(int p) {
+    if (p==0) return 2;
+    else if (p==2) return 0;
+    else if (p==3) return 1;
+    else return 3;
+}
+
+void moveBackTwoStep(int p) {
+    if (p==0) r+=2;
+    else if (p==1) c-=2;
+    else if (p==2) r-=2;
+    else c+=2;
+}
+
+void movePosition(int cnt, int p) {
+    if (cnt == 5) {
+        int b = backDirection(p);
+        if (arr[r+dY[b]][c+dX[b]] != 0) {
+            isExit = true;
+        } else {
+            moveBackTwoStep(p);
         }
-
-        // cout << "[DFS] cur:" << cur << ", cnt:" << cnt << endl;
-        // cout << "[DFS] startTeam.size: " << startTeam.size() << endl;
-        // cout << "[DFS] linkTeam.size: " << linkTeam.size() << endl;
-
-        int startStat=0, linkStat=0;
-        for (int i=0; i<startTeam.size(); i++) {
-            for (int j=i+1; j<startTeam.size(); j++) {
-                int startX = startTeam[i], startY = startTeam[j];
-                int linkX = linkTeam[i], linkY = linkTeam[j];
-                startStat += S[startX][startY] + S[startY][startX];
-                linkStat += S[linkX][linkY] + S[linkY][linkX];
-            }
-        }
-        
-        answer = min(answer, abs(startStat - linkStat));
         return;
     }
 
-    for (int i=cur+1; i<N; i++) {
-        if (!chk[i])  {
-            chk[i] = true;
-            DFS(i, cnt+1);
-            chk[i] = false;
-        }
+    int nD = calDirection(p);
+
+
+    int nR = r + dY[nD];
+    int nC = c + dX[nD];
+
+    if (arr[nR][nC] == 0) {
+        r = nR;
+        c = nC;
+    } else {
+        movePosition(cnt+1, nD);
     }
+    return;
 }
 
 int main() {
-    cin.tie(NULL); cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+    int res = 0;
 
-    cin >> N;
+    cin >> N >> M;
+    cin >> r >> c >> d;
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    while(1) {
+        arr[r][c] = -1;
+        movePosition(0, d);
+        if (isExit) break;
+    }
+
     for (int i=0; i<N; i++)
-        for (int j=0; j<N; j++)
-            cin >> S[i][j];
+        for (int j=0; j<M; j++)
+            if (arr[i][j] == -1) res++;
 
-    DFS(0, 0);
-    cout << answer;
-
-
+    cout << res;
     return 0;
 }
--- C++/tempCodeRunnerFile.cpp
+++ C++/tempCodeRunnerFile.cpp
@@ -1,7 +1,94 @@
-    // do {
-    //     bool ok = true;
-    //     int sum = 0;
-    //     for (int i=0; i<n-1; i++) {
-    //         if ()
-    //     }
-    // }
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#define MAX 9
+using namespace std;
+
+int N, M;
+int zeroCnt = 0;
+int maxZero = 0;
+int arr[MAX][MAX], tmp[MAX][MAX];
+int dX[4] = {0, 1, 0, -1}, dY[4] = {1, 0, -1, 0};
+queue<pair<int, int>> virusQ;
+
+bool isInRange(int X, int Y) {
+    return (0<=X && X<M) && (0<=Y && Y<N);
+}
+
+void BFS(int& zero) {
+    int afterWall[MAX][MAX];
+    memcpy(afterWall, tmp, sizeof(tmp));
+    queue<pair<int, int>> Q = virusQ;
+    int _zero = zero;
+
+    while(!Q.empty()) {
+        int X = Q.front().second;
+        int Y = Q.front().first;
+        Q.pop();
+
+        for (int i=0; i<4; i++) {
+            int nX = X + dX[i];
+            int nY = Y + dY[i];
+
+            if (!isInRange(nX, nY)) continue; 
+
+            if (afterWall[nY][nX] == 0) {
+                afterWall[nY][nX] = 2;
+                _zero--;
+                Q.push({nY, nX});
+            }
+        }
+    }
+    maxZero = max(maxZero, _zero);
+}
+
+void makeWall (int cnt, int& zero) {
+    if (cnt == 3) {
+        BFS(zero);
+        return;
+    }
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            if (tmp[i][j] == 0) {
+                tmp[i][j] = 1;
+                zero--;
+                makeWall(cnt + 1, zero);
+                tmp[i][j] = 0;
+                zero++;
+            }
+        }
+    }
+}
+
+
+int main () {
+
+    cin >> N >> M;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 0) zeroCnt++;
+            else if (arr[i][j] == 2) {
+                virusQ.push({i, j});
+            }
+        }
+    }
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            if (arr[i][j] == 0) {
+                memcpy(tmp, arr, sizeof(arr));
+                tmp[i][j] = 1;
+                zeroCnt--;
+                makeWall(1, zeroCnt);
+                tmp[i][j] = 0;
+                zeroCnt++;
+            }
+        }
+    }
+
+    cout << maxZero;
+
+    return 0;
+}
