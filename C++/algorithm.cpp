32931a0f06e20bd2a2946e266e3038f21658da93
Hyeon <antaehyeon@live.co.kr>
2019-08-09 01:19:22 +0900

    Feat: 톱니바퀴(14891) C++ 풀이 (틀림)

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,82 +1,133 @@
 #include <iostream>
 #include <algorithm>
 #include <deque>
-#define MAX 101
+#include <stdio.h>
+
+#define cw 1
+#define ccw -1
+
 
 using namespace std;
 
-int n, x, y, d, g, ans;
-int map[MAX][MAX] = {0, };
-int dX[4] = {1, 0, -1, 0};
-int dY[4] = {0, -1, 0, 1};
-deque<int> dq;
+int k, a, b, ans=0;
+bool isSpin[5] = {false, };
+int isDirection[5] = {0, };
+deque<int> w[5];
 
-
-void Print(deque<int> deque) {
-    while (!deque.empty()) {
-        cout << deque.front() << " ";
-        deque.pop_front();
+void Input() {
+    for(int i=1; i<5; i++) {
+        for (int j=0; j<8; j++) {
+            int t;
+            scanf("%1d", &t);
+            w[i].push_back(t);
+        }
     }
-    cout << '\n';
 }
 
-void PrintMap() {
-    for (int i=0; i<MAX; i++) {
+void Print() {
+    cout << "\n";
+    for (int i=1; i<5; i++) {
+        for (int j=0; j<8; j++) {
+            cout << w[i].at(j) << " ";
+        }
         cout << "\n";
-        for (int j=0; j<MAX; j++) {
-            cout << map[i][j] << " ";
-        }
     }
 }
 
-void makePath() {
-    dq.push_back(d);
-
-    for (int i=1; i<=g; i++) {
-        for (int j=dq.size(); j>0; j--) {
-            int b = dq.at(j-1)+1 == 4 ? 0 : dq.at(j-1)+1;
-            // cout << "[push:" << b << "]\n";
-            dq.push_back(b);
-        }
-        // Print(dq);
-    }
-    // cout << "[dq_size:" << dq.size() << "]\n";
-}
-
-void move() {
-    map[y][x] = 1;
-    while(!dq.empty()) {
-        int t = dq.front();
-        dq.pop_front();
-        x = x + dX[t];
-        y = y + dY[t];
-        map[y][x] = 1;
+void Spin(int idx, int mode) {
+    // cout << "Spin [idx:" << idx << "][mode:" << mode << "]\n";
+    if (mode == cw) {
+        int t = w[idx].back();
+        w[idx].pop_back();
+        w[idx].push_front(t);
+    } else {
+        int t = w[idx].front();
+        w[idx].pop_front();
+        w[idx].push_back(t);
     }
 }
 
-void checkSquare() {
-    for (int i=0; i<100; i++) {
-        for (int j=0; j<100; j++) {
-            if (map[i][j] == 1 && map[i][j+1] == 1 && map[i+1][j] == 1 && map[i+1][j+1] == 1) ans++;
+void processSpin() {
+    for (int i=1; i<5; i++) {
+        if (isSpin[i]) {
+            Spin(i, isDirection[i]);
         }
     }
+    for (int i=1; i<5; i++) {
+        isSpin[i] = false;
+        isDirection[i] = 0;
+    }
+}
+
+void left(int current, int mode) {
+    // cout << "left [current:" << current << "][mode:" << mode << "]\n";
+    // if (current < 1) return;
+
+    if (current == 1) {
+        isSpin[current] = true;
+        isDirection[current] = mode;
+        return;
+    }
+
+    int cwv = w[current].at(6);
+    int pwv = w[current-1].at(2);
+
+    // cout << "LEFT [cwv:" << cwv << "][pwv:" << pwv << "]\n";
+
+    if (cwv != pwv) {
+        left(current-1, (mode == cw) ? ccw : cw);
+        isSpin[current] = true;
+        isDirection[current] = mode;
+    }
+}
+
+void right(int current, int mode) {
+    // cout << "right [current:" << current << "][mode:" << mode << "]\n";
+    // if (current >= 4) return;
+    
+    if (current == 4) {
+        isSpin[current] = true;
+        isDirection[current] = mode;
+        return;
+    }
+
+    int cwv = w[current].at(2);
+    int pwv = w[current+1].at(6);
+
+    // cout << "RIGHT [cwv:" << cwv << "][pwv:" << pwv << "]\n";
+
+    if (cwv != pwv) {
+        right(current+1, (mode == cw) ? ccw : cw);
+        isSpin[current] = true;
+        isDirection[current] = mode;
+    }
+}
+
+void calResult() {
+    // N-0, S-1
+    if (w[1].at(0) == 1) ans += 1;
+    if (w[2].at(0) == 1) ans += 2;
+    if (w[3].at(0) == 1) ans += 4;
+    if (w[4].at(0) == 1) ans += 8;
 }
 
 int main() {
+    Input();
+    // Print();
 
-    cin >> n;
+    cin >> k;
+    for (int i=0; i<k; i++) {
+        cin >> a >> b;
+        left(a, b);
+        right(a, b);
+        isSpin[a] = true;
+        isDirection[a] = b;
+        processSpin();
 
-    for (int i=0; i<n; i++) {
-        cin >> x >> y >> d >> g;
-        makePath();
-        move();
+        // Print();
     }
 
-    checkSquare();
+    calResult();
 
-    // PrintMap();
-
-    cout << ans << "\n";
-
-    return 0;
+    cout << ans;
 }
