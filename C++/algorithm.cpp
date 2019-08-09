d9897bf3557524dd8671fada06e9f51310f52f83
Hyeon <antaehyeon@live.co.kr>
2019-08-09 23:22:29 +0900

    Feat: 테트로미노(14500) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,139 +1,74 @@
 #include <iostream>
 #include <algorithm>
-#include <deque>
-#include <stdio.h>
-
-#define cw 1
-#define ccw -1
-
-
+#define MAX 500
 using namespace std;
 
-int k, a, b, ans=0;
-bool isSpin[5] = {false, };
-int isDirection[5] = {0, };
-deque<int> w[5];
+int n, m;
+int dY[4] = {0, 1, 0, -1};
+int dX[4] = {1, 0, -1, 0};
+int map[MAX][MAX];
+bool visited[MAX][MAX] = {false, };
 
-void Input() {
-    for(int i=1; i<5; i++) {
-        for (int j=0; j<8; j++) {
-            int t;
-            scanf("%1d", &t);
-            w[i].push_back(t);
+bool isInArrange(int y, int x) {
+    return (0<=y && y<n) && (0<=x && x<m);
+}
+
+int DFS(int y, int x, int cnt) {
+    if (cnt == 4) return map[y][x];
+    int sum = 0;
+    for (int i=0; i<4; i++) {
+        int nextY = y + dY[i];
+        int nextX = x + dX[i];
+
+        if (isInArrange(nextY, nextX) && !visited[nextY][nextX]) {
+            visited[nextY][nextX] = true;
+            sum = max(sum, map[y][x] + DFS(nextY, nextX, cnt+1));
+            visited[nextY][nextX] = false;
         }
     }
+    return sum;
 }
 
-void Print() {
-    cout << "\n";
-    for (int i=1; i<5; i++) {
-        for (int j=0; j<8; j++) {
-            cout << w[i].at(j) << " ";
-        }
-        cout << "\n";
+int middle(int y, int x) {
+    int res=0;
+
+    if (y>=1 && x>=1 && x<m-1) {
+        res=max(res, map[y][x] + map[y][x-1] + map[y][x+1] + map[y-1][x]);
     }
-}
-
-void Spin(int idx, int mode) {
-    // cout << "Spin [idx:" << idx << "][mode:" << mode << "]\n";
-    if (mode == cw) {
-        int t = w[idx].back();
-        w[idx].pop_back();
-        w[idx].push_front(t);
-    } else {
-        int t = w[idx].front();
-        w[idx].pop_front();
-        w[idx].push_back(t);
+    if (y>=1 && y<n-1 && x<m-1) {
+        res = max(res, map[y][x] + map[y-1][x] + map[y+1][x] + map[y][x+1]);
     }
-}
-
-void processSpin() {
-    for (int i=1; i<5; i++) {
-        if (isSpin[i]) {
-            Spin(i, isDirection[i]);
-        }
+    if (x>=1 && y<n-1 && x<m-1) {
+        res = max(res, map[y][x] + map[y][x-1] + map[y][x+1] + map[y+1][x]);
     }
-    for (int i=1; i<5; i++) {
-        isSpin[i] = false;
-        isDirection[i] = 0;
+    if (x>=1 && y>=1 && y<n-1) {
+        res = max(res, map[y][x] + map[y-1][x] + map[y+1][x] + map[y][x-1]);
     }
-}
 
-void left(int current, int mode) {
-    // cout << "left [current:" << current << "][mode:" << mode << "]\n";
-    // if (current < 1) return;
-
-    // if (current == 1) {
-    //     isSpin[current] = true;
-    //     isDirection[current] = mode;
-    //     return;
-    // }
-    if (!(1<=current && current<=4)) return;
-
-    int cwv = w[current+1].at(6);
-    int pwv = w[current].at(2);
-
-    // cout << "left [cwv:" << cwv << "][pwv:" << pwv << "]\n";
-
-    if (cwv != pwv) {
-        left(current-1, (mode == cw) ? ccw : cw);
-        // isSpin[current] = true;
-        // isDirection[current] = mode;
-        Spin(current, (mode == cw) ? ccw : cw);
-    }
-}
-
-void right(int current, int mode) {
-    // cout << "right [current:" << current << "][mode:" << mode << "]\n";
-    // if (current >= 4) return;
-
-    if (!(1<=current && current<=4)) return;
-    
-    // if (current == 4) {
-    //     isSpin[current] = true;
-    //     isDirection[current] = mode;
-    //     return;
-    // }
-
-    int cwv = w[current-1].at(2);
-    int nwv = w[current].at(6);
-
-    // cout << "right [cwv:" << cwv << "][nwv:" << nwv << "]\n";
-
-    if (cwv != nwv) {
-        right(current+1, (mode == cw) ? ccw : cw);
-        // isSpin[current] = true;
-        // isDirection[current] = mode;
-        Spin(current, (mode == cw) ? ccw : cw);
-    }
-}
-
-void calResult() {
-    // N-0, S-1
-    if (w[1].front() == 1) ans += 1;
-    if (w[2].front() == 1) ans += 2;
-    if (w[3].front() == 1) ans += 4;
-    if (w[4].front() == 1) ans += 8;
+    return res;
 }
 
 int main() {
-    Input();
-    // Print();
-    // cout << "\n";
 
-    cin >> k;
-    for (int i=0; i<k; i++) {
-        cin >> a >> b;
-        left(a-1, b);
-        right(a+1, b);
-        // isSpin[a] = true;
-        // isDirection[a] = b;
-        // processSpin();
-        Spin(a, b);
-        // Print();
+    cin >> n >> m;
+    
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin >> map[i][j];
+        }
     }
 
-    calResult();
+    int result = 0;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            visited[i][j] = true;
+            result = max(result, DFS(i, j, 1));
+            result = max(result, middle(i, j));
+            visited[i][j] = false;
+        }
+    }
 
-    cout << ans;
+    cout << result;
+
+    return 0;
 }
