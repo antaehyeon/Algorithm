88752c2c94d4167cd42df2ae239d024338e0975f
Hyeon <antaehyeon@live.co.kr>
2019-08-12 22:48:24 +0900

    Feat: 백준 암호만들기(1759) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,74 +1,50 @@
 #include <iostream>
 #include <algorithm>
-#define MAX 500
+#include <utility>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-int n, m;
-int dY[4] = {0, 1, 0, -1};
-int dX[4] = {1, 0, -1, 0};
-int map[MAX][MAX];
-bool visited[MAX][MAX] = {false, };
+int l, c;
+vector<char> alphabet;
 
-bool isInArrange(int y, int x) {
-    return (0<=y && y<n) && (0<=x && x<m);
+bool check(string pw) {
+    int mo=0, ja=0;
+    for (auto w : pw) {
+        if (w=='a' || w=='e' || w=='i' || w=='o' || w=='u') mo++;
+        else ja++;
+    }
+
+    return ja>=2 && mo>=1;
 }
 
-int DFS(int y, int x, int cnt) {
-    if (cnt == 4) return map[y][x];
-    int sum = 0;
-    for (int i=0; i<4; i++) {
-        int nextY = y + dY[i];
-        int nextX = x + dX[i];
 
-        if (isInArrange(nextY, nextX) && !visited[nextY][nextX]) {
-            visited[nextY][nextX] = true;
-            sum = max(sum, map[y][x] + DFS(nextY, nextX, cnt+1));
-            visited[nextY][nextX] = false;
-        }
+void makePW(string pw, int cnt) {
+    // cout << "[pw.length:" << pw.length() << "][pw:" << pw << "]\n";
+    if (pw.length() == l) {
+        if (check(pw)) cout << pw << "\n";
+        return;
     }
-    return sum;
-}
-
-int middle(int y, int x) {
-    int res=0;
-
-    if (y>=1 && x>=1 && x<m-1) {
-        res=max(res, map[y][x] + map[y][x-1] + map[y][x+1] + map[y-1][x]);
-    }
-    if (y>=1 && y<n-1 && x<m-1) {
-        res = max(res, map[y][x] + map[y-1][x] + map[y+1][x] + map[y][x+1]);
-    }
-    if (x>=1 && y<n-1 && x<m-1) {
-        res = max(res, map[y][x] + map[y][x-1] + map[y][x+1] + map[y+1][x]);
-    }
-    if (x>=1 && y>=1 && y<n-1) {
-        res = max(res, map[y][x] + map[y-1][x] + map[y+1][x] + map[y][x-1]);
-    }
-
-    return res;
+    if (cnt >= c) return;
+    makePW(pw+alphabet[cnt], cnt+1);
+    makePW(pw, cnt+1);
 }
 
 int main() {
 
-    cin >> n >> m;
-    
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            cin >> map[i][j];
-        }
+    cin >> l >> c;
+
+    for (int i=0; i<c; i++) {
+        char tmp;
+        cin >> tmp;
+        alphabet.push_back(tmp);
     }
 
-    int result = 0;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            visited[i][j] = true;
-            result = max(result, DFS(i, j, 1));
-            result = max(result, middle(i, j));
-            visited[i][j] = false;
-        }
-    }
+    sort(alphabet.begin(), alphabet.end());
 
-    cout << result;
+    makePW("", 0);
 
     return 0;
 }
+
