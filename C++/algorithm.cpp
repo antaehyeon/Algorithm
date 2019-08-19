0df08ae93aec82445bc49b66168a979458721531
Hyeon <antaehyeon@live.co.kr>
2019-08-20 04:29:34 +0900

    Feat: 백준 부분순열의 합(1182) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,101 +1,36 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
+#include <utility>
 #include <vector>
-#define MAX 100
+#include <queue>
 
 using namespace std;
 
-int r, c, k, ans=-1;
-int map[MAX][MAX] = {0, };
-int cntArr[MAX] = {0, };
+int a[20];
+int n, m;
+int ans = 0;
 
-void Print() {
-    cout << "#####################################\n";
-    for (int i=0; i<MAX; i++) {
-        cout << "\n";
-        for (int j=0; i<MAX; j++) {
-            cout << map[i][j] << " ";
-        }
-    }
-    cout << "#####################################\n";
+void go(int i, int sum) {
+	if (i == n) {
+		if (sum == m) ans += 1;
+		return;
+	}
+	go(i + 1, sum + a[i]);
+	go(i + 1, sum);
 }
 
-void Input() {
-    cin >> r >> c >> k;
-    for(int i=0; i<3; i++) {
-        for (int j=0; j<3; j++) {
-            cin >> map[i][j];
-        }
-    }
-}
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-void Solution() {
-    int hang=3, yul=3;
-    int size = 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
 
-    if (hang >= yul) {
-        for (int i=0; i<hang; i++) {
-            vector<pair<int ,int>> V;
-            memset(cntArr, 0, sizeof(cntArr));
-            for (int j=0; j<yul; j++) cntArr[map[i][j]]++;
-            for (int j=0; j<MAX; j++) {
-                if (cntArr[j] == 0) continue;
-                V.push_back(make_pair(cntArr[j], j));
-            }
-            sort(V.begin(), V.end());
-            for (int j=0; j<yul; j++) map[i][j]=0;
-            size = size > V.size() ? size : V.size();
-            for (int j=0; j<size; j+=2) {
-                int cnt=V[j/2].first;
-                int num=V[j/2].second;
-                map[i][j]=num;
-                map[i][j+1]=cnt;
-            }
-        }
-    } else {
-        for (int i=0; i<yul; i++) {
-            vector<pair<int ,int>> V;
-            memset(cntArr, 0, sizeof(cntArr));
-            for (int j=0; j<hang; j++) cntArr[map[j][i]]++;
-            for (int j=0; j<MAX; j++) {
-                if (cntArr[j] == 0) continue;
-                V.push_back(make_pair(cntArr[j], j));
-            }
-            sort(V.begin(), V.end());
-            for (int j=0; j<hang; j++) map[j][i]=0;
-            size = size > V.size() ? size : V.size();
-            for (int j=0; j<size; j+=2) {
-                int cnt=V[j/2].first;
-                int num=V[j/2].second;
-                map[j][i] = num;
-                map[j+1][i] = cnt;
-            }
-        }
-    }
-    ans++;
-}
+	go(0, 0);
+	if (m == 0) ans -= 1;
+	cout << ans << "\n";
 
-bool Check() {
-    if (map[r][c] == k) return true;
-    else if (ans > 100) {
-        ans = -1;
-        return true;
-    }
-    return false;
-}
-
-int main () {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
-    Input();
-    while(1) {
-        if (Check()) break;
-        Solution();
-        // Print();
-    }
-
-    cout << ans;
-    return 0;
+	return 0;
 }
