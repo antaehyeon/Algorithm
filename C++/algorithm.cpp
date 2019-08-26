2aed457a0b70ea83add3ce11dfb72444528eefd7
Hyeon <antaehyeon@live.co.kr>
2019-08-27 03:35:51 +0900

    Feat: 백준 이차원 배열과 연산(17140) C++ 풀이 (2차 틀림)

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,80 +1,101 @@
 #include <iostream>
 #include <algorithm>
-#define MAX 22
-
-int n, m, x, y, k;
-int map[MAX][MAX];
-int command[1002];
-int dice[7] = { 0, };
-
-int dx[4] = { 1, -1, 0 ,0 };
-int dy[4] = { 0, 0, -1, 1 };
+#include <cstring>
+#include <vector>
+#define MAX 100
 
 using namespace std;
 
-void move() {
-	for (int i = 0; i < k; i++) {
-		int direction = command[i];
-		int floorN = map[y][x];
+int r, c, k, ans=-1;
+int map[MAX][MAX] = {0, };
+int cntArr[MAX] = {0, };
 
-		int nextX = x + dx[direction - 1];
-		int nextY = y + dy[direction - 1];
-
-		if (nextX < 0 || nextX >= m || nextY < 0 || nextY >= n) continue;
-
-		x = nextX; y = nextY;
-
-		int top = dice[1];
-		if (direction == 1) {
-			dice[1] = dice[4];
-			dice[4] = dice[6];
-			dice[6] = dice[3];
-			dice[3] = top;
-		}
-		else if (direction == 2) {
-			dice[1] = dice[3];
-			dice[3] = dice[6];
-			dice[6] = dice[4];
-			dice[4] = top;
-		}
-		else if (direction == 3) {
-			dice[1] = dice[5];
-			dice[5] = dice[6];
-			dice[6] = dice[2];
-			dice[2] = top;
-		}
-		else {
-			dice[1] = dice[2];
-			dice[2] = dice[6];
-			dice[6] = dice[5];
-			dice[5] = top;
-		}
-
-		if (map[y][x] == 0) map[y][x] = dice[6];
-		else {
-			dice[6] = map[y][x];
-			map[y][x] = 0;
-		}
-
-		cout << dice[1];
-		if (i != k - 1) cout << "\n";
-	}
+void Print() {
+    cout << "#####################################\n";
+    for (int i=0; i<MAX; i++) {
+        cout << "\n";
+        for (int j=0; i<MAX; j++) {
+            cout << map[i][j] << " ";
+        }
+    }
+    cout << "#####################################\n";
 }
 
-int main() {
+void Input() {
+    cin >> r >> c >> k;
+    for(int i=0; i<3; i++) {
+        for (int j=0; j<3; j++) {
+            cin >> map[i][j];
+        }
+    }
+}
 
-	cin >> n >> m >> y >> x >> k;
+void Solution() {
+    int hang=3, yul=3;
+    int size = 0;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
-		}
-	}
-	for (int i = 0; i < k; i++) {
-		cin >> command[i];
-	}
+    if (hang >= yul) {
+        for (int i=0; i<hang; i++) {
+            vector<pair<int ,int>> V;
+            memset(cntArr, 0, sizeof(cntArr));
+            for (int j=0; j<yul; j++) cntArr[map[i][j]]++;
+            for (int j=0; j<MAX; j++) {
+                if (cntArr[j] == 0) continue;
+                V.push_back(make_pair(cntArr[j], j));
+            }
+            sort(V.begin(), V.end());
+            for (int j=0; j<yul; j++) map[i][j]=0;
+            size = size > V.size() ? size : V.size();
+            for (int j=0; j<size; j+=2) {
+                int cnt=V[j/2].first;
+                int num=V[j/2].second;
+                map[i][j]=num;
+                map[i][j+1]=cnt;
+            }
+        }
+    } else {
+        for (int i=0; i<yul; i++) {
+            vector<pair<int ,int>> V;
+            memset(cntArr, 0, sizeof(cntArr));
+            for (int j=0; j<hang; j++) cntArr[map[j][i]]++;
+            for (int j=0; j<MAX; j++) {
+                if (cntArr[j] == 0) continue;
+                V.push_back(make_pair(cntArr[j], j));
+            }
+            sort(V.begin(), V.end());
+            for (int j=0; j<hang; j++) map[j][i]=0;
+            size = size > V.size() ? size : V.size();
+            for (int j=0; j<size; j+=2) {
+                int cnt=V[j/2].first;
+                int num=V[j/2].second;
+                map[j][i] = num;
+                map[j+1][i] = cnt;
+            }
+        }
+    }
+    ans++;
+}
 
-	move();
+bool Check() {
+    if (map[r][c] == k) return true;
+    else if (ans > 100) {
+        ans = -1;
+        return true;
+    }
+    return false;
+}
 
-	return 0;
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    Input();
+    while(1) {
+        if (Check()) break;
+        Solution();
+        // Print();
+    }
+
+    cout << ans;
+    return 0;
 }
