7cf6c7bb1250494c7549a619feb9404a3a783a3e
Hyeon <antaehyeon@live.co.kr>
2019-08-28 00:57:01 +0900

    Feat: 백준 감시(15683) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,101 +1,149 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 #include <vector>
-#define MAX 100
+#include <utility>
+#include <string.h>
+#define MAX 8
 
 using namespace std;
 
-int r, c, k, ans=-1;
-int map[MAX][MAX] = {0, };
-int cntArr[MAX] = {0, };
+int n, m, ans = 987654321;
+int map[MAX][MAX];
+vector <pair<int, pair<int, int>>> cctvList;
 
-void Print() {
-    cout << "#####################################\n";
-    for (int i=0; i<MAX; i++) {
-        cout << "\n";
-        for (int j=0; i<MAX; j++) {
-            cout << map[i][j] << " ";
-        }
-    }
-    cout << "#####################################\n";
+void Print(int arr[MAX][MAX]) {
+	cout << "#########" << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
 }
 
-void Input() {
-    cin >> r >> c >> k;
-    for(int i=0; i<3; i++) {
-        for (int j=0; j<3; j++) {
-            cin >> map[i][j];
-        }
-    }
+void detect(int visited[][MAX], int y, int x, int dir) {
+	switch (dir) {
+	case 0: // right
+		for (int i = x; i < m; i++) {
+			if (visited[y][i] == 6) break;
+			visited[y][i] = 7;
+		}
+		break;
+	case 1: // bottom
+		for (int i = y; i < n; i++) {
+			if (visited[i][x] == 6) break;
+			visited[i][x] = 7;
+		}
+		break;
+	case 2: // left
+		for (int i = x; i >= 0; i--) {
+			if (visited[y][i] == 6) break;
+			visited[y][i] = 7;
+		}
+		break;
+	case 3: // top
+		for (int i = y; i >= 0; i--) {
+			if (visited[i][x] == 6) break;
+			visited[i][x] = 7;
+		}
+		break;
+	}
 }
 
-void Solution() {
-    int hang=3, yul=3;
-    int size = 0;
+void search(int idx, int prev[][MAX]) {
+	int visited[MAX][MAX];
 
-    if (hang >= yul) {
-        for (int i=0; i<hang; i++) {
-            vector<pair<int ,int>> V;
-            memset(cntArr, 0, sizeof(cntArr));
-            for (int j=0; j<yul; j++) cntArr[map[i][j]]++;
-            for (int j=0; j<MAX; j++) {
-                if (cntArr[j] == 0) continue;
-                V.push_back(make_pair(cntArr[j], j));
-            }
-            sort(V.begin(), V.end());
-            for (int j=0; j<yul; j++) map[i][j]=0;
-            size = size > V.size() ? size : V.size();
-            for (int j=0; j<size; j+=2) {
-                int cnt=V[j/2].first;
-                int num=V[j/2].second;
-                map[i][j]=num;
-                map[i][j+1]=cnt;
-            }
-        }
-    } else {
-        for (int i=0; i<yul; i++) {
-            vector<pair<int ,int>> V;
-            memset(cntArr, 0, sizeof(cntArr));
-            for (int j=0; j<hang; j++) cntArr[map[j][i]]++;
-            for (int j=0; j<MAX; j++) {
-                if (cntArr[j] == 0) continue;
-                V.push_back(make_pair(cntArr[j], j));
-            }
-            sort(V.begin(), V.end());
-            for (int j=0; j<hang; j++) map[j][i]=0;
-            size = size > V.size() ? size : V.size();
-            for (int j=0; j<size; j+=2) {
-                int cnt=V[j/2].first;
-                int num=V[j/2].second;
-                map[j][i] = num;
-                map[j+1][i] = cnt;
-            }
-        }
-    }
-    ans++;
+
+	if (idx == cctvList.size()) {
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (prev[i][j] == 0) cnt++;
+			}
+		}
+
+		if (ans > cnt) ans = cnt;
+	}
+	else {
+		pair<int, pair<int, int>> p = cctvList.at(idx);
+		int y = p.first;
+		int x = p.second.first;
+		int type = p.second.second;
+
+		switch (type) {
+		case 1:
+			for (int i = 0; i < 4; i++) {
+				for (int k = 0; k < n; k++) {
+					memcpy(visited[k], prev[k], sizeof(prev[k]));
+				}
+				detect(visited, y, x, i);
+				search(idx + 1, visited);
+			}
+			break;
+		case 2:
+			for (int i = 0; i < 2; i++) {
+				for (int k = 0; k < n; k++) {
+					memcpy(visited[k], prev[k], sizeof(prev[k]));
+				}
+				detect(visited, y, x, i);
+				detect(visited, y, x, i + 2);
+				search(idx + 1, visited);
+			}
+			break;
+		case 3:
+			for (int i = 0; i < 4; i++) {
+				for (int k = 0; k < n; k++) {
+					memcpy(visited[k], prev[k], sizeof(prev[k]));
+				}
+				detect(visited, y, x, i);
+				detect(visited, y, x, (i + 1) % 4);
+				search(idx + 1, visited);
+			}
+			break;
+		case 4:
+			for (int i = 0; i < 4; i++) {
+				for (int k = 0; k < n; k++) {
+					memcpy(visited[k], prev[k], sizeof(prev[k]));
+				}
+				detect(visited, y, x, i);
+				detect(visited, y, x, (i + 1) % 4);
+				detect(visited, y, x, (i + 2) % 4);
+				search(idx + 1, visited);
+			}
+			break;
+		case 5:
+			for (int k = 0; k < n; k++) {
+				memcpy(visited[k], prev[k], sizeof(prev[k]));
+			}
+			detect(visited, y, x, 0);
+			detect(visited, y, x, 1);
+			detect(visited, y, x, 2);
+			detect(visited, y, x, 3);
+			search(idx + 1, visited);
+			break;
+		}
+	}
 }
 
-bool Check() {
-    if (map[r][c] == k) return true;
-    else if (ans > 100) {
-        ans = -1;
-        return true;
-    }
-    return false;
+
+int main() {
+	
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+			if (map[i][j] != 0 && map[i][j] != 6) {
+				cctvList.push_back(make_pair(i, make_pair(j, map[i][j])));
+			}
+		}
+	}
+
+	search(0, map);
+
+	cout << ans;
+
+	return 0;
 }
 
-int main () {
-    ios::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
-    Input();
-    while(1) {
-        if (Check()) break;
-        Solution();
-        // Print();
-    }
-
-    cout << ans;
-    return 0;
-}
