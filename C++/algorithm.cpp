fd9726adb5e49a33745d1872ef94880841b55181
Hyeon <antaehyeon@live.co.kr>
2019-08-29 04:27:46 +0900

    Feat: 백준 ABCDE(13023) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,90 +1,53 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <utility>
-#include <math.h>
-#define MAX 51
-#define INF 987654321
+#include <string.h>
+#define MAX 2001
 
 using namespace std;
 
-int n, m, ans = INF;
-int map[MAX][MAX];
-vector<pair<int, int>> homeList;
-vector<pair<int, int>> chickenList;
-vector<vector<int>> permutation;
+int n, m, ans = 0;
+bool visit[MAX] = { false, };
+vector<int> graph[MAX];
+bool isAns = false;
 
-int calAbs(int a, int b, int c, int d) {
-	int t1 = a > b ? (a - b) : (b - a);
-	int t2 = c > d ? (c - d) : (d - c);
 
-	return t1 + t2;
-}
-
-void cal(vector<pair<int, int>> v) {
-
-	int res = 0;
-	int cnt = INF;
-
-	for (pair<int, int> home : homeList) {
-		for (pair<int, int> c : v) {
-			cnt = min(cnt, calAbs(home.first, c.first, home.second, c.second));
-		}
-		res += cnt;
-		cnt = INF;
+void DFS(int start, int cnt) {
+	if (cnt == 4) {
+		isAns = true; return;
 	}
-
-	ans = min(ans, res);
-}
-
-
-void solution(vector<int> v) {
-
-	vector<pair<int, int>> cl;
-	for (int idx : v) {
-		cl.push_back(make_pair(chickenList[idx].first, chickenList[idx].second));
+	visit[start] = true;
+	for (int i = 0; i < graph[start].size(); i++) {
+		int next = graph[start][i];
+		if (visit[next]) continue;
+		DFS(next, cnt + 1);
 	}
-	cal(cl);
+	visit[start] = false;
 }
-
-void check() {
-	vector<int> tmp;
-	int chickenListN = chickenList.size();
-
-	if (chickenListN > m) {
-		for (int i = 0; i < chickenListN - m; i++) {
-			tmp.push_back(0);
-		}
-	}
-
-	for (int i = 0; i < m; i++) tmp.push_back(1);
-
-	do {
-		vector<int> selChickenIdx;
-		for (int i = 0; i < chickenListN; i++) {
-			if (tmp[i] == 1) selChickenIdx.push_back(i);
-		}
-		solution(selChickenIdx);
-	} while (next_permutation(tmp.begin(), tmp.end()));
-}
-
 
 
 int main() {
-
 	cin >> n >> m;
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 1) homeList.push_back(make_pair(i, j));
-			else if (map[i][j] == 2) chickenList.push_back(make_pair(i, j));
-		}
+	for (int i = 0; i < m; i++) {
+		int v1, v2;
+		cin >> v1 >> v2;
+		graph[v1].push_back(v2);
+		graph[v2].push_back(v1);
 	}
 
-	check();
+	for (int i = 0; i < n; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
 
-	cout << ans;
+	for (int i = 0; i < n; i++) {
+		memset(visit, false, sizeof(visit)); ans = 0;
+		DFS(i, 0);
+		if (isAns) break;
+	}
+
+	if (isAns) cout << 1 << endl;
+	else cout << 0 << endl;
 
 	return 0;
 }
+
