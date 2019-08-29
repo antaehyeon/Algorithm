07f6750c9015a6b3b6d9b90ed67a2fc40ff3b215
Hyeon <antaehyeon@live.co.kr>
2019-08-30 03:50:31 +0900

    Feat: 백준 이분그래프 (1707) C++ 풀이 (틀림)

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#define MAX 1001
+#include <string.h>
+#define MAX 200001
 
 using namespace std;
 
-int n, m, ans = 0;
+int k, n, m;
+bool isAnswer = true;
 bool visit[MAX];
+int flag[MAX] = { -1, };
+int f = 1;
 vector<int> graph[MAX];
+vector<string> answerList;
 
 void input() {
 	int v1, v2;
@@ -20,36 +25,60 @@ void input() {
 
 void DFS(int start) {
 	visit[start] = true;
+	flag[start] = f;
+	f = f == 1 ? 2 : 1;
 
 	for (int i = 0; i < graph[start].size(); i++) {
 		int next = graph[start][i];
+		if (flag[next] == flag[start]) {
+			isAnswer = false;
+			return;
+		}
+
 		if (!visit[next]) {
 			DFS(next);
 		}
 	}
 }
 
-int check() {
-	for (int i = 1; i <= n; i++) {
-		if (!visit[i]) {
-			ans++;
-			return i;
+void reset() {
+	for (int i = 1; i < MAX; i++) {
+		graph[i].erase(graph[i].begin(), graph[i].end());
+	}
+	memset(visit, false, sizeof(visit));
+	memset(flag, -1, sizeof(flag));
+	f = 1;
+	isAnswer = true;
+}
+
+void storeAnswer() {
+	if (isAnswer) answerList.push_back("YES");
+	else answerList.push_back("NO");
+}
+
+void printAnswer() {
+	int cnt = 0;
+	for (auto ans : answerList) {
+		cnt++;
+		if (cnt == answerList.size()) {
+			cout << ans;
+		}
+		else {
+			cout << ans << endl;
 		}
 	}
-	return -1;
 }
 
 int main() {
-	cin >> n >> m;
-	input();
-
-	while (1) {
-		int idx = check();
-		if (idx == -1) break;
-		DFS(idx);
+	cin >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> n >> m;
+		if (i!=0) reset();
+		input();
+		DFS(1);
+		storeAnswer();
 	}
-
-	cout << ans;
+	printAnswer();
 
 	return 0;
 }
