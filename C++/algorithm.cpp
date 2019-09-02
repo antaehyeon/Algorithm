3764b66de42a0edcb42939d090ed6c04deb156d1
Hyeon <antaehyeon@live.co.kr>
2019-09-03 01:49:48 +0900

    Feat: 백준 섬의 갯수(4963) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,79 +1,82 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#define MAX 25
+#include <string.h>
+#define endl "\n"
+#define MAX 50
 
 using namespace std;
 
-int n, cnt = 0;
-vector<int> v;
+int w, h, ans = 0;
 int map[MAX][MAX];
 
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0 ,0, 1, -1 };
+int dx[8] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+int dy[8] = { 0, 0, 1, -1, -1, -1, 1, 1 };
 
 void input() {
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%1d", &map[i][j]);
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			cin >> map[i][j];
 		}
 	}
 }
 
 bool isAccessable(int y, int x) {
-	return (y >= 0 && y < n) && (x >= 0 && x < n);
+	return (y >= 0 && y < h) && (x >= 0 && x < w);
 }
 
 void DFS(int y, int x) {
 	map[y][x] = 2;
-	for (int i = 0; i < 4; i++) {
+
+	for (int i = 0; i < 8; i++) {
 		int nextX = x + dx[i];
 		int nextY = y + dy[i];
 
 		if (!isAccessable(nextY, nextX)) continue;
 
 		if (map[nextY][nextX] == 1) {
-			cnt++;
 			DFS(nextY, nextX);
 		}
 	}
 }
 
 void solution() {
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			int data = map[i][j];
-
-			if (data == 1) {
-				cnt++;
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			
+			if (map[i][j] == 1) {
+				ans++;
 				DFS(i, j);
-				v.push_back(cnt);
-				cnt = 0;
 			}
+
 		}
 	}
-
 }
 
-void print() {
-
-	cout << v.size() << endl;
-
-	sort(v.begin(), v.end());
-
-	for (auto i : v) {
-		cout << i << endl;
+void reset() {
+	for (int i = 0; i < h; i++) {
+		memset(map[i], 0, sizeof(map[i]));
 	}
-
+	ans = 0;
 }
 
 int main() {
-	input();
-	solution();
-	print();
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	return 0;
+	while (1) {
+		cin >> w >> h;
+
+		if (w == 0 && h == 0) break;
+
+		input();
+		solution();
+		cout << ans << endl;
+		reset();
+	}
+
+	
+
+
+
 }
