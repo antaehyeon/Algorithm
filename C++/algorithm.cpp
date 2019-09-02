f6c761e8242f623f1cc941aea9533b7010d42668
Hyeon <antaehyeon@live.co.kr>
2019-09-03 04:19:46 +0900

    Feat: 백준 토마토 (7576) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <algorithm>
+#include <utility>
 #include <string.h>
 #include <queue>
 #define endl "\n"
-#define MAX 101
+#define MAX 1001
 
 using namespace std;
 
-int n, m;
+int n, m, ans = 0;
 int map[MAX][MAX];
+queue<pair<int, int>> q;
 
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
 
 void input() {
-	cin >> n >> m;
+	cin >> m >> n;
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
-			scanf("%1d", &map[i][j]);
+			cin >> map[i][j];
+			if (map[i][j] == 1) {
+				q.push(make_pair(i, j));
+			}
 		}
 	}
 }
@@ -29,9 +34,6 @@ bool isAccessable(int y, int x) {
 
 void BFS() {
 	int cv = 0;
-	queue<pair<int, int>> q;
-
-	q.push(make_pair(1, 1));
 
 	while (!q.empty()) {
 		int cx = q.front().second;
@@ -45,7 +47,7 @@ void BFS() {
 
 			if (!isAccessable(ny, nx)) continue;
 
-			if (map[ny][nx] == 1) {
+			if (map[ny][nx] == 0) {
 				map[ny][nx] = cv + 1;
 				q.push(make_pair(ny, nx));
 			}
@@ -53,15 +55,31 @@ void BFS() {
 	}
 }
 
+void print() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (map[i][j] == 0) {
+				ans = -1;
+				return;
+			}
+			else {
+				ans = max(ans, map[i][j]);
+			}
+		}
+	}
+}
+
 
 int main() {
-	// ios_base::sync_with_stdio(false);
-	// cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
 	input();
 	BFS();
+	print();
 
-	cout << map[n][m] << endl;
+	ans = ans == -1 ? -1 : ans - 1;
+	cout << ans << endl;
 
 	return 0;
 }
