d752445e26fcd7be99b59cf30cb85c28c72ac74f
Hyeon <antaehyeon@live.co.kr>
2019-09-14 23:58:44 +0900

    Feat: 백준 아기상어 (16236) C++ 풀이 (틀림)

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,58 +1,151 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <queue>
 #include <utility>
-#include <string>
-
-int ans = 987654321;
+#include <cstring>
+#define MAX 21
 
 using namespace std;
 
-void solution(string &str, int k) {
-	int strSize = str.length();
-	int cnt = 0;
-	bool isFlag = false;
-	string prevStr = str.substr(0, k);
-	string nextStr = "";
+int n;
+int map[MAX][MAX], tmp[MAX][MAX];
+int size = 2;
+int time = 0;
+bool isEnd = false;
+int upgradeCnt = 0;
 
-	if (k == strSize) {
-		ans = ans > cnt ? cnt : ans;
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+struct pos {
+	int y;
+	int x;
+	int s;
+	int d;
+};
+
+vector<pos> sharks;
+pos mainShark;
+
+
+void input() {
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 9) {
+				mainShark.y = i;
+				mainShark.x = j;
+				mainShark.s = 2;
+			}
+		}
+	}
+}
+
+bool isAccessable(int y, int x) {
+	return (y >= 0 && y < n) && (x >= 0 && x < n);
+}
+
+bool compare(pos p1, pos p2) {
+
+	// return (p1.x < p2.x) && (p1.y < p2.y) && (p1.s < p2.s);
+
+	return p1.x < p2.x;
+}
+
+bool compare2(pos p1, pos p2) {
+	return p1.y < p2.y;
+}
+
+bool compare3(pos p1, pos p2) {
+	return p1.d < p2.d;
+}
+
+void reset() {
+	// vector<pos> sv;
+	// sharks.swap(sv);
+	// sharks.erase(sharks.begin(), sharks.end() + 1);
+	sharks.clear();
+
+	for (int i = 0; i < n; i++) {
+		memset(tmp[i], 0, sizeof(tmp[i]));
+	}
+}
+
+void BFS() {
+	queue<pos> list;
+	list.push({ mainShark.y, mainShark.x, mainShark.s });
+
+	while (!list.empty()) {
+		int x = list.front().x;
+		int y = list.front().y;
+		int v = tmp[y][x];
+		list.pop();
+		
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (!isAccessable(ny, nx)) continue;
+			if (tmp[ny][nx] > 0) continue;
+			if (map[ny][nx] > mainShark.s) continue;
+			if (map[ny][nx] > 0 && map[ny][nx] != 9 && map[ny][nx] != mainShark.s) {
+				sharks.push_back({ ny, nx, map[ny][nx], v+1 });
+			}
+			tmp[ny][nx] = v + 1;
+			list.push({ ny, nx, map[ny][nx] });
+		}
+	}
+}
+
+void cal() {
+	if (sharks.size() == 0) {
+		isEnd = true;
 		return;
 	}
 
-	for (int i = k; i < strSize; i++) {
-		nextStr += str[i];
-		if (i % k == 0) {
-			if (prevStr.length() > strSize - i) {
-				cnt += (strSize - i);
-			}
+	sort(sharks.begin(), sharks.end(), compare);
+	sort(sharks.begin(), sharks.end(), compare2);
+	sort(sharks.begin(), sharks.end(), compare3);
 
-			if (prevStr != nextStr) {
-				if (!isFlag) cnt += k;
-				isFlag = false;
-			}
-			else if (prevStr == nextStr) {
-				if (!isFlag) cnt += (k + 1);
-				isFlag = true;
-			}
-
-			prevStr = nextStr;
-			nextStr = "";
-		}
+	if (mainShark.s > sharks[0].s) {
+		map[mainShark.y][mainShark.x] = 0;
+		map[sharks[0].y][sharks[0].x] = mainShark.s;
+		upgradeCnt++;
+		time += sharks[0].d;
+	}
+	else {
+		isEnd = true;
+		return;
 	}
 
-	ans = ans > cnt ? cnt : ans;
+	mainShark.y = sharks[0].y;
+	mainShark.x = sharks[0].x;
+	
+	if (upgradeCnt == mainShark.s) {
+		mainShark.s = upgradeCnt + 1;
+		upgradeCnt = 0;
+	}
 }
 
-int main() {
+void solution() {
 
-	string t1 = "aabbaccc";
-
-	for (int i = 1; i <= 8; i++) {
-		solution(t1, i);
+	while (true) {
+		BFS();
+		cal();
+		if (isEnd) break;
+		reset();
 	}
+}
 
-	cout << ans << endl;
 
+int main() {
+	input();
+	solution();
+
+	cout << time;
 
 	return 0;
 }
