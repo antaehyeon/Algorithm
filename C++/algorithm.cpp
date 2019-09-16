1c9700547ca94aaa65ef97ec54afba58e07b6559
Hyeon <antaehyeon@live.co.kr>
2019-09-16 23:52:32 +0900

    Feat: 백준 전기가 부족해(10423) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,128 +1,63 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <queue>
-#include <utility>
-#include <cstring>
-#include <unordered_set>
-#define MAX 100
+#include <algorithm>
+#define MAX 1001
 
 using namespace std;
 
-struct pos {
-	int y;
-	int x;
+int n, m, k, cost = 0;
+int vRoot[MAX] = { 0, };
+
+struct Tunnel {
+	int i;
+	int j;
+	int w;
 };
 
-bool isEnd = false;
-int n, l, r;
-int map[MAX][MAX];
-bool visit[MAX][MAX];
-bool isTemp = false;
-int ans = 0;
-int sum = 0;
-int cnt = 0;
-vector<pos> land;
+vector<Tunnel> tunnel;
 
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
+int findRoot(int v) {
+	if (vRoot[v] == -1) return -1;
+	else if (vRoot[v] == v) return v;
+	return vRoot[v] = findRoot(vRoot[v]);
+}
 
-void input() {
-	cin >> n >> l >> r;
+bool compare(Tunnel t1, Tunnel t2) {
+	return t1.w < t2.w;
+}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> map[i][j];
+void kruskal() {
+	sort(tunnel.begin(), tunnel.end(), compare);
+	for (Tunnel t : tunnel) {
+		int rootI = findRoot(t.i);
+		int rootJ = findRoot(t.j);
+		if (rootI != rootJ) {
+			if (rootI == -1) vRoot[rootJ] = rootI;
+			else if (rootJ == -1) vRoot[rootI] = rootJ;
+			else vRoot[rootJ] = rootI;
+			cost += t.w;
 		}
 	}
-}
-
-bool isAccessable(int y, int x) {
-	return (y >= 0 && y < n) && (x >= 0 && x < n);
-}
-
-void BFS(int y, int x) {
-	if (visit[y][x]) return;
-	queue<pos> q;
-
-	q.push({ y, x });
-
-	while (!q.empty()) {
-		int cy = q.front().y;
-		int cx = q.front().x;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int ny = cy + dy[i];
-			int nx = cx + dx[i];
-
-			if (!isAccessable(ny, nx)) continue;
-			if (visit[ny][nx]) continue;
-
-			int diff = abs(map[cy][cx] - map[ny][nx]);
-
-			if (diff >= l && diff <= r) {
-				land.push_back({ ny ,nx });
-				visit[ny][nx] = true;
-				sum += map[ny][nx];
-				cnt++;
-				q.push({ ny, nx });
-			}
-		}
-	}
-}
-
-void process() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (visit[i][j]) {
-				map[i][j] = (sum / cnt);
-			}
-		}
-	}
-}
-
-void reset() {
-	ans++;
-	cnt = 0;
-	sum = 0;
-	memset(visit, false, sizeof(visit));
-}
-
-void calc() {
-	if (land.size() == 0) return;
-	int c = 0;
-	int s = 0;
-	for (pos l : land) {
-		c++;
-		s += map[l.y][l.x];
-	}
-	int value = s / c;
-	for (pos l : land) {
-		map[l.y][l.x] = value;
-	}
-	land.clear();
-}
-
-void solution() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			BFS(i, j);
-			calc();
-		}
-	}
+	cout << cost;
 }
 
 int main() {
-	input();
-	while (true) {
-		solution();
-		if (cnt == 0) break;
-		//process();
-		reset();
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> m >> k;
+
+	for (int i = 1; i <= n; i++) vRoot[i] = i;
+	for (int i = 0; i < k; i++) {
+		int idx;
+		cin >> idx;
+		vRoot[idx] = -1;
 	}
-
-	cout << ans;
-
+	for (int k = 0; k < m; k++) {
+		int i, j, w;
+		cin >> i >> j >> w;
+		tunnel.push_back({ i, j, w });
+	}
+	kruskal();
 	return 0;
 }
