db57b058c136c7e810737d1a8b6015309a74e51f
An, Taehyeon <antaehyeon@live.co.kr>
2019-09-20 22:58:56 +0900

    Feat: 백준 ATM(11399) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,63 +1,34 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#define MAX 1001
+#include <vector>
+#include <queue>
+#include <cstring>
 
 using namespace std;
 
-int n, m, k, cost = 0;
-int vRoot[MAX] = { 0, };
-
-struct Tunnel {
-	int i;
-	int j;
-	int w;
-};
-
-vector<Tunnel> tunnel;
-
-int findRoot(int v) {
-	if (vRoot[v] == -1) return -1;
-	else if (vRoot[v] == v) return v;
-	return vRoot[v] = findRoot(vRoot[v]);
-}
-
-bool compare(Tunnel t1, Tunnel t2) {
-	return t1.w < t2.w;
-}
-
-void kruskal() {
-	sort(tunnel.begin(), tunnel.end(), compare);
-	for (Tunnel t : tunnel) {
-		int rootI = findRoot(t.i);
-		int rootJ = findRoot(t.j);
-		if (rootI != rootJ) {
-			if (rootI == -1) vRoot[rootJ] = rootI;
-			else if (rootJ == -1) vRoot[rootI] = rootJ;
-			else vRoot[rootJ] = rootI;
-			cost += t.w;
-		}
-	}
-	cout << cost;
-}
+int n;
+int p = 0;
+int sum = 0;
+vector<int> v;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	
-	cin >> n >> m >> k;
 
-	for (int i = 1; i <= n; i++) vRoot[i] = i;
-	for (int i = 0; i < k; i++) {
-		int idx;
-		cin >> idx;
-		vRoot[idx] = -1;
-	}
-	for (int k = 0; k < m; k++) {
-		int i, j, w;
-		cin >> i >> j >> w;
-		tunnel.push_back({ i, j, w });
-	}
-	kruskal();
-	return 0;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        int t;
+        cin >> t;
+        v.push_back(t);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i=0; i<n; i++) {
+        p += v[i];
+        sum += p;
+    }
+
+    cout << sum << endl;
+
+    return 0;
 }
