b738b9fb97d2355dc79254a5b636ae6e5027228e
An, Taehyeon <antaehyeon@live.co.kr>
2019-09-20 22:49:11 +0900

    Feat: 프로그래머스 나머지 한점 JS풀이

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,24 +1,26 @@
-function solution(phone_book) {
-  let answer = false;
 
-  phone_book.sort();
+const posX = [];
+const posY = [];
+const X = 0;
+const Y = 1;
 
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    for (let j = i + 1; j < phone_book.length; j++) {
-      if (phone_book[i].startsWith(phone_book[j])) return answer;
-      if (phone_book[j].startsWith(phone_book[i])) return answer;
+const solution = (v) => {
+    const ans = [];
+
+    for (const pos of v) {
+        if (posX.includes(pos[X])) posX.splice(posX.findIndex(value => value===pos[X]), 1);
+        else posX.push(pos[X])
+
+        if (posY.includes(pos[Y])) posY.splice(posY.findIndex(value => value === pos[Y]), 1);
+        else posY.push(pos[Y])
     }
-  }
 
-  answer = true;
+    ans.push(posX[0]);
+    ans.push(posY[0]);
 
-  return answer;
+    return ans;
 }
 
-const t1 = ["119", "97674223", "1195524421"];
-const t2 = ["12", "123", "1235", "567", "88"];
-const t3 = ["123", "456", "789"];
 
-console.log(solution(t1));
-console.log(solution(t2));
-console.log(solution(t3));
+const res = solution([[1, 4], [3, 4], [3, 10]]);
+console.log(res);
