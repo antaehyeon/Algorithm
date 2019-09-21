417fa910ae28443b341fcb3ec93dc0a26a2256f0
HYEON(iMac) <antaehyeon@live.co.kr>
2019-09-21 19:15:13 +0900

    Feat: 네이버 2019 하반기 1일차 3번문제 C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -3,36 +3,81 @@
 #include <vector>
 #include <queue>
 #include <cstring>
+#include <deque>
+#include <string>
+#define MAX 1000
 
 using namespace std;
 
-int n, sum = 0, avg = 0, ans = -987654321;
-vector<int> nv;
+struct Doc
+{
+	int pi;
+	int start;
+	int pn;
+};
 
-bool compare(int n1, int n2) {
-	return n1 > n2;
+deque<Doc> docV;
+
+int _time = 0;
+
+bool compare(Doc d1, Doc d2) {
+	if (d1.pn <= d2.pn) {
+		if (d1.pn == d2.pn) {
+			return d1.start < d2.start;
+		}
+		return true;
+	}
+	return false;
 }
 
+void solution3(vector<vector<int>> data) {
+	int idx = 1;
+	vector<int> ans;
+
+	ans.push_back(1);
+	_time += data[0][2]+1;
+
+	while (data.size() != ans.size()) {
+		if (_time >= data[idx][1]) {
+			docV.push_back({ data[idx][0], data[idx][1], data[idx][2] });
+			++idx;
+		}
+		else {
+
+			if (docV.empty()) {
+				_time++;
+				continue;
+			}
+
+			cout << endl;
+			sort(docV.begin(), docV.end(), compare);
+
+			for (auto t : docV) {
+				cout << "[i: " << t.pi << " start: " << t.start << " pn: " << t.pn << "]\n";
+			}
+
+			_time += (docV.front().pn+1);
+			cout << "[time: " << _time << "]\n";
+			ans.push_back(docV.front().pi);
+			docV.pop_front();
+		}
+	}
+
+	for (int seq : ans) {
+		cout << seq << " ";
+	}
+}
+
+string map[MAX][MAX];
+
+
 int main() {
-	cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		int t;
-		cin >> t;
-		sum += t;
-		nv.push_back(t);
-	}
+	solution3({ {1, 0, 5}, {2, 2, 2}, {3, 3, 1}, {4, 4, 1}, {5, 10, 2} });
+	// solution3({ {1,0,3}, {2,1,3}, {3,3,2}, {4,9,1}, {5,10,2} });
+	// solution3({ {1,2,10}, {2,5,8}, {3,6,9}, {4,20,6}, {5,25,5} });
 
-	avg = sum / n;
-
-	sort(nv.begin(), nv.end(), compare);
-
-	for (int i = 1; i <= n; i++) {
-		int temp = nv[i-1] * i;
-		ans = ans > temp ? ans : temp;
-	}
-
-	cout << ans;
+	// solution2({ "######", ">#*###", "####*#", "#<#>>#", ">#*#*<", "######" });
 	
 	return 0;
 }
