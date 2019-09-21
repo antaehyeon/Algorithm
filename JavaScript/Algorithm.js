00f7897cfb744d93001b317a02c6b2ad85416e32
HYEON(iMac) <antaehyeon@live.co.kr>
2019-09-22 02:57:24 +0900

    Feat: 라인 2018 하반기 1번 문제 JS 풀이

--- JavaScript/Algorithm.js
+++ JavaScript/Algorithm.js
@@ -1,18 +1,31 @@
-const checkName = name => {};
+let card = 20000;
 
 const solution = param => {
-  let ans = 0;
-  const regex = /[\w+\.?\w+]+\@\w+\.[com|net|org]+/g;
+  let price = 720;
 
-  for (email of param) {
-    const res = email.replace(regex, "");
-    console.log(res);
-
-    if (res === "") ans++;
+  console.log(param);
+  if (param > 40) {
+    const a = Math.floor((param - 40) / 8);
+    const b = param % 8;
+    console.log(`a: ${a}, b: ${b}`);
+    price += a * 80;
+    if (b > 0) price += 80;
+    console.log(price);
   }
 
-  console.log(ans);
+  return price;
 };
 
-solution(["d@co@m.com", "a@abc.com", "b@def.com", "c@ghi.net"]);
-solution(["abc.def@x.com", "abc", "abc@defx", "abc@defx.xyz"]);
+const main = () => {
+  const data = "11 128 15 111 59 31 70 102 50 172 88 56 40 41 12";
+  const splited = data.split(" ");
+  let sum = 0;
+
+  for (const n of splited) {
+    sum += solution(n);
+  }
+
+  console.log(20000 - sum);
+};
+
+main();
