e83228560d6e20befd330970d48e93a75c097ae2
HYEON(iMac) <antaehyeon@live.co.kr>
2019-09-22 00:04:07 +0900

    Feat: 백준 대회 or 인턴 (2875) C++ 풀이

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,30 +1,11 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
 
 using namespace std;
 
-bool compare(char a, char b) {
-	return a > b;
-}
-
 int main() {
-	string str;
-	cin >> str;
-	long long sum = 0;
-	bool zero = false;
+	int n, m, k;
+	cin >> n >> m >> k;
 
-	for (auto c : str) {
-		sum += (c - '0');
-		if (!(c - '0')) zero = true;
-	}
-
-	if (sum % 3 || !zero) cout << -1;
-	else {
-		sort(str.begin(), str.end(), compare);
-		cout << str;
-	}
-
-
-	return 0;
+	cout << min(min(n / 2, m), (n + m - k) / 3);
 }
