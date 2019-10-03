685d0db1565add453cfa4933fa8b64c901ca4eaa
hyeon(macbook pro) <antaehyeon@live.co.kr>
2019-10-03 17:34:28 +0900

    Feat: 백준 아기상어(17143) C++ 풀이 (시간초과)

--- C++/algorithm.cpp
+++ C++/algorithm.cpp
@@ -1,110 +1,145 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <string>
-#define MAX 1000
+#include <queue>
+#include <cstring>
+#define MAX 101
 
 using namespace std;
 
-struct pos {
-	char mode;
-	int degree;
-};
+int r, c, m, ans = 0;
+int dx[5] = {0, 0, 0, 1, -1};
+int dy[5] = {0, -1, 1, 0, 0};
 
-struct loc {
-	int x;
+struct Fish
+{
 	int y;
+	int x;
+	int s; // speed
+	int d; // direction
+	int z; // size
 };
 
-int n;
-pos map[MAX][MAX];
-bool visit[MAX][MAX];
-char curr;
+queue<Fish> fq;
+vector<Fish> map[MAX][MAX];
 
-int dx[4] = { 0, 1, 0, -1 };
-int dy[4] = { -1, 0, 1, 0 };
-
-char next(char p, char c) {
-	if (p == 'F') return c;
-
-	if (p == 'R') {
-		if (c == 'F') return 'R';
-		if (c == 'R') return 'B';
-		if (c == 'L') return 'F';
-		if (c == 'B') return 'L';
-	}
-
-	if (p == 'L') {
-		if (c == 'F') return 'L';
-		if (c == 'R') return 'F';
-		if (c == 'L') return 'B';
-		if (c == 'B') return 'R';
-	}
-
-	if (p == 'B') {
-		if (c == 'F') return 'B';
-		if (c == 'R') return 'L';
-		if (c == 'L') return 'R';
-		if (c == 'B') return 'F';
+void input()
+{
+	cin >> r >> c >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int y, x, s, d, z;
+		cin >> y >> x >> s >> d >> z;
+		map[y][x].push_back({y, x, s, d, z});
 	}
 }
 
-loc adjustDirection(char mode, int degree, loc l) {
-
-	if (mode == 'F') l.y -= degree;
-	else if (mode == 'R') l.x += degree;
-	else if (mode == 'L') l.x -= degree;
-	else if (mode == 'B') l.y += degree;
-
-	return l;
-}
-
-void solution() {
-	int prev = 'B';
-	int x = 0;
-	int y = 0;
-
-	while (true) {
-
-		if (visit[y][x]) {
-			
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					cout << visit[i][j] << " ";
-				}
-				cout << endl;
+void findAndInitMap()
+{
+	for (int i = 1; i <= r; i++)
+	{
+		for (int j = 1; j <= c; j++)
+		{
+			if (map[i][j].size())
+			{
+				fq.push(map[i][j][0]);
+				map[i][j].clear();
 			}
-
-			cout << "[x: " << x << " y: " << y << "]\n";
-			break;
-		}
-
-		visit[y][x] = true;
-		char _curr = map[y][x].mode;
-		cout << "[_curr: " << (char)_curr << "]\n";
-		prev = next(prev, _curr);
-		cout << "[next prev: " << (char)prev << "]\n";
-
-		int _degree = map[y][x].degree;
-		loc _nextL = adjustDirection(prev, _degree, { x, y });
-		x = _nextL.x;
-		y = _nextL.y;
-	}
-}
-
-void input() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			string s;
-			cin >> s;
-			map[i][j] = { s[0], s[1] - '0' };
 		}
 	}
 }
 
-int main() {
+bool isAccessable(int y, int x)
+{
+	return (y > 0 && y <= r) && (x > 0 && x <= c);
+}
+
+int changeDirection(int dir)
+{
+	if (dir == 1)
+		return 2;
+	else if (dir == 2)
+		return 1;
+	else if (dir == 3)
+		return 4;
+	else if (dir == 4)
+		return 3;
+	else
+		return -1;
+}
+
+void moveFish()
+{
+	findAndInitMap();
+	while (!fq.empty())
+	{
+		Fish fish = fq.front();
+		int y = fish.y;
+		int x = fish.x;
+		int size = fish.z;
+		int speed = fish.s;
+		int direction = fish.d;
+		fq.pop();
+
+		for (int i = 0; i < speed; i++)
+		{
+			int ny = y + dy[direction];
+			int nx = x + dx[direction];
+
+			if (!isAccessable(ny, nx))
+			{
+				direction = changeDirection(direction);
+				i--;
+				continue;
+			}
+			else
+			{
+				y = ny;
+				x = nx;
+			}
+		}
+
+		if (map[y][x].size())
+		{
+			if (map[y][x][0].z < size)
+			{
+				map[y][x].clear();
+				map[y][x].push_back({y, x, speed, direction, size});
+			}
+		}
+		else
+		{
+			map[y][x].push_back({y, x, speed, direction, size});
+		}
+	}
+}
+
+void solution()
+{
+	for (int x = 1; x <= c; x++)
+	{
+		for (int y = 1; y <= r; y++)
+		{
+			if (map[y][x].size())
+			{
+				ans += map[y][x][0].z;
+				map[y][x].clear();
+				break;
+			}
+		}
+		moveFish();
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	input();
 	solution();
-}
+	cout << ans;
 
+	return 0;
+}
